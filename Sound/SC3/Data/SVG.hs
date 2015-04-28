38e5c5447759e208dbafb4e5472c25213861f61f
Rohan Drape <rd@slavepianos.org>
2015-04-28 04:51:03 +0000

    +pathcommand_to_abs,+path_to_abs

--- Sound/SC3/Data/SVG.hs
+++ Sound/SC3/Data/SVG.hs
@@ -1,6 +1,7 @@
 -- | Load linearised path data from SVG files.
 module Sound.SC3.Data.SVG where
 
+import Data.List {- base -}
 import Data.Maybe {- base -}
 import System.IO.Unsafe {- base -}
 import qualified Text.XML.Light as X {- xml -}
@@ -34,18 +35,49 @@ svg_read_path_d s =
         d = mapMaybe (X.findAttr (X.unqual "d")) p
     in map parse_path d
 
-subpaths_to_ls :: (Double,Double) -> [P.PathCommand] -> [Ls Double]
+subpaths_to_ls :: (R,R) -> [P.PathCommand] -> [Ls R]
 subpaths_to_ls (dx,dy) r =
     case P.commandsToPoints r (dx,dy) (0,0) of
       [] -> error "subpaths_to_ls: no sub-paths"
       p -> map (map pt') p
 
-svg_load_ls :: (Double, Double) -> FilePath -> IO [Ls Double]
+svg_load_ls :: (R, R) -> FilePath -> IO [Ls R]
 svg_load_ls rs fn = do
   s <- readFile fn
   let d = svg_read_path_d s
       p = concatMap (subpaths_to_ls rs) d
   return p
 
+-- | 'plotCoord' of 'pt_xy'.
 plot_ls :: PNum t => [Ls t] -> IO ()
 plot_ls = plotCoord . map (map pt_xy)
+
+-- | Convert a relative 'P.PathCommand' to the absolute form, giving
+-- also the absolute end point.
+pathcommand_to_abs :: (R,R) -> P.PathCommand -> ((R,R),P.PathCommand)
+pathcommand_to_abs (x,y) cmd =
+    case cmd of
+      P.M_abs (x',y') -> let r = (x',y') in (r,P.M_abs r)
+      P.M_rel (x',y') -> let r = (x+x',y+y') in (r,P.M_abs r)
+      P.Z -> ((x,y),P.Z)
+      P.L_abs (x',y') -> let r = (x',y') in (r,P.L_abs r)
+      P.L_rel (x',y') -> let r = (x+x',y+y') in (r,P.L_abs r)
+      P.H_abs x' -> let r = (x',y) in (r,P.H_abs x')
+      P.H_rel x' -> let x'' = x+x' ; r = (x'',y) in (r,P.H_abs x'')
+      P.V_abs y' -> let r = (x,y') in (r,P.V_abs y')
+      P.V_rel y' -> let y'' = y+y' ; r = (x,y'') in (r,P.V_abs y'')
+      P.C_abs (x1,y1,x2,y2,x',y') -> ((x',y'),P.C_abs (x1,y1,x2,y2,x',y'))
+      P.C_rel (x1,y1,x2,y2,x',y') -> ((x+x',y+y'),P.C_abs (x+x1,y+y1,x+x2,y+y2,x+x',y+y'))
+      P.S_abs (x2,y2,x',y') -> ((x',y'),P.S_abs (x2,y2,x',y'))
+      P.S_rel (x2,y2,x',y') -> ((x+x',y+y'),P.S_abs (x+x2,y+y2,x+x',y+y'))
+      P.Q_abs (x1,y1,x',y') -> ((x',y'),P.Q_abs (x1,y1,x',y'))
+      P.Q_rel (x1,y1,x',y') -> ((x+x',y+y'),P.Q_abs (x+x1,y+y1,x+x',y+y'))
+      P.T_abs (x',y') -> let r = (x',y') in (r,P.T_abs r)
+      P.T_rel (x',y') -> let r = (x+x',y+y') in (r,P.T_abs r)
+      P.A_abs -> ((x,y),P.A_abs) -- error "A_abs"
+      P.A_rel -> ((x,y),P.A_rel) -- error "A_rel"
+
+-- | Convert a sequence of 'P.PathCommand' to absolute form given
+-- starting point.
+path_to_abs :: (R,R) -> [P.PathCommand] -> ((R,R),[P.PathCommand])
+path_to_abs = mapAccumL pathcommand_to_abs
