7ab9822aed82cbd44159a575aa9bbf2b4c0f207f
Rohan Drape <rd@slavepianos.org>
2015-10-21 13:12:19 +0000

    ~{au|sf}-to-img (+mode=bw|gs)

--- cmd/au-to-img.hs
+++ cmd/au-to-img.hs
@@ -4,15 +4,25 @@ import System.FilePath {- filepath -}
 import qualified Sound.File.NeXT as AU {- hsc3-sf -}
 import Sound.SC3.Data.Image.Plain {- hsc3-data -}
 
-au_to_img :: FilePath -> IO ()
-au_to_img fn = do
+au_to_img :: (Double -> Double) -> FilePath -> IO ()
+au_to_img op fn = do
   (hdr,ch) <- AU.read fn
-  let img = img_from_gs (AU.frameCount hdr,AU.channelCount hdr) ch
+  let img = img_from_gs (AU.frameCount hdr,AU.channelCount hdr) (map (map op) ch)
   img_write_png (fn <.> "png") img
+
+help :: IO ()
+help = putStrLn "au-to-img {bw|gs} file-name"
+
+md_to_fn :: String -> (Double -> Double)
+md_to_fn md =
+    case md of
+      "bw" -> (1 -)
+      "gs" -> id
+      _ -> error "md = {bw|gs}"
 
 main :: IO ()
 main = do
   a <- getArgs
   case a of
-    [fn] -> au_to_img fn
-    _ -> putStrLn "au-to-img file-name"
+    [md,fn] -> au_to_img (md_to_fn md) fn
+    _ -> help
--- cmd/sf-to-img.hs
+++ cmd/sf-to-img.hs
@@ -1,21 +1,32 @@
+import qualified Data.Vector.Storable as V {- vector -}
 import System.Environment {- base -}
 import System.FilePath {- filepath -}
 
 import qualified Sound.File.HSndFile as SF {- hsc3-sf-hsndfile -}
 import Sound.SC3.Data.Image.Plain {- hsc3-data -}
 
-sf_to_img :: FilePath -> IO ()
-sf_to_img fn = do
+sf_to_img :: (Double -> Double) -> FilePath -> IO ()
+sf_to_img op fn = do
   (hdr,Just vec) <- SF.read_vec fn
-  let img = img_from_vec_co (SF.frameCount hdr,SF.channelCount hdr) vec
+  let img = img_from_vec_co (SF.frameCount hdr,SF.channelCount hdr) (V.map op vec)
   img_write_png (fn <.> "png") img
+
+help :: IO ()
+help = putStrLn "sf-to-img {bw|gs} file-name"
+
+md_to_fn :: String -> (Double -> Double)
+md_to_fn md =
+    case md of
+      "bw" -> (1 -)
+      "gs" -> id
+      _ -> error "md = {bw|gs}"
 
 main :: IO ()
 main = do
   a <- getArgs
   case a of
-    [fn] -> sf_to_img fn
-    _ -> putStrLn "sf-to-img file-name"
+    [md,fn] -> sf_to_img (md_to_fn md) fn
+    _ -> help
 
 {-
 
