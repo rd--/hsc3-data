3e2ffc8877ece7ee8ce129957a51bf7ead221671
Rohan Drape <rd@slavepianos.org>
2015-11-02 05:33:26 +0000

    ~cmd/sf-draw (+normalise mode)

--- cmd/sf-draw.hs
+++ cmd/sf-draw.hs
@@ -6,11 +6,20 @@ import qualified Sound.File.HSndFile as SF {- hsc3-sf-hsndfile -}
 import qualified Sound.SC3 as S {- hsc3 -}
 import qualified Sound.SC3.Data.Bitmap.PBM as P {- hsc3-data -}
 
-vec_normalise :: (Fractional b, Ord b, V.Storable b) => V.Vector b -> V.Vector b
-vec_normalise v =
+vec_normalise_1 :: (Fractional b, Ord b, V.Storable b) => V.Vector b -> V.Vector b
+vec_normalise_1 v =
     let v' = V.map abs v
         m = V.foldl1 max v'
     in V.map (\e -> S.linlin' e (- m) m (-1) 1) v
+
+vec_normalise_2 :: (Fractional b, Ord b, V.Storable b) => V.Vector b -> V.Vector b
+vec_normalise_2 v =
+    let l = V.foldl1 min v
+        r = V.foldl1 max v
+    in V.map (\e -> S.linlin' e l r (-1) 1) v
+
+vec_normalise :: (Fractional b, Ord b, V.Storable b) => Bool -> V.Vector b -> V.Vector b
+vec_normalise hlf = if hlf then vec_normalise_2 else vec_normalise_1
 
 vec_ix :: Bool -> Double -> Int -> Int -> V.Vector Double -> Int -> [(Int,Int)]
 vec_ix sym h ch nc vec i =
@@ -18,12 +27,12 @@ vec_ix sym h ch nc vec i =
         f x = round (((x + 1) / 2) * (h - 1))
     in if sym then [(f v,i),(f (negate v),i)] else [(f v,i)]
 
-sf_draw :: (Bool,Bool,Bool) -> Int -> Int -> FilePath -> FilePath -> IO ()
-sf_draw (nrm,inv,sym) h ch sf_fn pbm_fn = do
+sf_draw :: (Bool,Bool,Bool,Bool) -> Int -> Int -> FilePath -> FilePath -> IO ()
+sf_draw (nrm,hlf,inv,sym) h ch sf_fn pbm_fn = do
   (hdr,Just vec') <- SF.read_vec sf_fn
   let vec = ((if sym then V.map abs else id) .
              (if inv then id else V.map negate) .
-             (if nrm then vec_normalise else id)) vec'
+             (if nrm then vec_normalise hlf else id)) vec'
       nc = SF.channelCount hdr
       nf = SF.frameCount hdr
       ix = vec_ix sym (fromIntegral h) ch nc vec
@@ -31,32 +40,40 @@ sf_draw (nrm,inv,sym) h ch sf_fn pbm_fn = do
   let b = ((h,nf),concatMap ix [0 .. nf - 1])
   P.pbm_write pbm_fn (P.bitindices_to_pbm b)
 
-help :: String
+help :: [String]
 help =
-    unwords
-    ["sf-draw"
-    ,"pbm"
-    ,"normalise:bool"
-    ,"invert:bool"
-    ,"symmetrical:bool"
-    ,"pbm-height:int"
-    ,"channel:int"
-    ,"sound-file"
-    ,"pbm-file"]
+    [unwords
+     ["sf-draw"
+     ,"pbm"
+     ,"normalise:bool"
+     ,"normalise-mode:bool"
+     ,"invert:bool"
+     ,"symmetrical:bool"
+     ,"pbm-height:int"
+     ,"channel:int"
+     ,"sound-file"
+     ,"pbm-file"]
+    ," normalise-mode: f = retain symmetry about zero, t = ignore symmetry"]
 
 main :: IO ()
 main = do
   a <- getArgs
   case a of
-    ["pbm",nrm,inv,sym,h,ch,sf_fn,pbm_fn] ->
-        sf_draw (nrm == "t",inv == "t",sym == "t") (read h) (read ch) sf_fn pbm_fn
-    _ -> putStrLn help
+    ["pbm",nrm,hlf,inv,sym,h,ch,sf_fn,pbm_fn] ->
+        sf_draw (nrm == "t",hlf == "t",inv == "t",sym == "t") (read h) (read ch) sf_fn pbm_fn
+    _ -> mapM_ putStrLn help
 
 {-
 
-let sf_fn = "/home/rohan/uc/sp-id/eof/au/tbl/gs/01.tbl.au"
 let sf_fn = "/home/rohan/data/audio/gebet.R.1024.au"
 let pbm_fn = "/tmp/t.pbm"
-sf_draw (False,False,True) 128 0 sf_fn pbm_fn
+sf_draw (False,False,False,True) 128 0 sf_fn pbm_fn
+
+let sf_fn = "/home/rohan/uc/sp-id/eof/au/tbl/gs/01.tbl.au"
+sf_draw (True,True,True,False) 32 0 sf_fn pbm_fn
+
+import System.Process
+rawSystem "display" [pbm_fn]
+rawSystem "convert" [pbm_fn,pbm_fn ++ ".png"]
 
 -}
