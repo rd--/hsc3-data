aba286218786d44ac4b214f8811c790f67d53a07
Rohan Drape <rd@slavepianos.org>
2015-11-19 04:17:52 +0000

    sf-to-img -> sf-to-png

--- cmd/sf-to-png.hs
+++ cmd/sf-to-png.hs
@@ -1,18 +1,17 @@
 import qualified Data.Vector.Storable as V {- vector -}
 import System.Environment {- base -}
-import System.FilePath {- filepath -}
 
 import qualified Sound.File.HSndFile as SF {- hsc3-sf-hsndfile -}
 import Sound.SC3.Data.Image.Plain {- hsc3-data -}
 
-sf_to_img :: (Double -> Double) -> FilePath -> IO ()
-sf_to_img op fn = do
-  (hdr,Just vec) <- SF.read_vec fn
-  let img = img_from_vec_co (SF.frameCount hdr,SF.channelCount hdr) (V.map op vec)
-  img_write_png (fn <.> "png") img
+sf_to_png :: (Double -> Double) -> FilePath -> FilePath -> IO ()
+sf_to_png op sf_fn png_fn = do
+  (hdr,Just vec) <- SF.read_vec sf_fn
+  let png = img_from_vec_co (SF.frameCount hdr,SF.channelCount hdr) (V.map op vec)
+  img_write_png png_fn png
 
 help :: IO ()
-help = putStrLn "sf-to-img {bw|gs} file-name"
+help = putStrLn "sf-to-png {bw|gs} sound-file png-file"
 
 md_to_fn :: String -> (Double -> Double)
 md_to_fn md =
@@ -25,13 +24,14 @@ main :: IO ()
 main = do
   a <- getArgs
   case a of
-    [md,fn] -> sf_to_img (md_to_fn md) fn
+    [md,sf_fn,png_fn] -> sf_to_png (md_to_fn md) sf_fn png_fn
     _ -> help
 
 {-
 
+:set -XScopedTypeVariables
 let fn = "/home/rohan/desert/01.bw.extract.png.au"
-(hdr,Just vec) <- SF.read_vec fn
+(hdr,Just (vec :: V.Vector Float)) <- SF.read_vec fn
 ERROR: out of memory (requested 17783848960 bytes)
 
 -}
---
+++ md/sf-to-png.md
@@ -0,0 +1,3 @@
+# sf-to-png
+
+Variant of `au-to-pgm`.
