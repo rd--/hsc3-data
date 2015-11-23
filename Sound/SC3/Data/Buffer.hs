b256a98501f00265eb28343196558873aad23348
Rohan Drape <rd@slavepianos.org>
2015-11-23 19:33:53 +0000

    ~b_tmp_allocRead (- hashable dep.)

--- Sound/SC3/Data/Buffer.hs
+++ Sound/SC3/Data/Buffer.hs
@@ -1,6 +1,5 @@
 module Sound.SC3.Data.Buffer where
 
-import Data.Hashable {- hashable -}
 import System.Directory {- directory -}
 import System.FilePath {- filepath -}
 
@@ -10,12 +9,12 @@ import Sound.SC3 {- hsc3 -}
 import qualified Sound.File.NeXT as F {- hsc3-sf -}
 
 -- | Message to send data to scsynth via temporary audio file.
-b_tmp_allocRead :: (Hashable n,Floating n,Real n) => Int -> [n] -> IO Message
+b_tmp_allocRead :: (Floating n,Real n) => Int -> [n] -> IO Message
 b_tmp_allocRead nid d = do
   tmp <- getTemporaryDirectory
   let nc = 1
       sr = 1
       h = F.Header (length d) F.Float sr nc
-      nm = tmp </> show (hash d) <.> "au"
+      nm = tmp </> "b_tmp_allocRead" <.> show nid <.> "au"
   F.au_write nm h [d]
   return (b_allocRead nid nm 0 0)
