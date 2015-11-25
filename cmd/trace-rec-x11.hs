006f4c20fa26175bfa606d2d1904922eec00353a
Rohan Drape <rd@slavepianos.org>
2015-11-25 06:48:19 +0000

    ~cmd/trace-rec-x11

--- cmd/trace-rec-x11.hs
+++ cmd/trace-rec-x11.hs
@@ -1,6 +1,7 @@
 import Control.Exception {- base -}
 import Control.Monad {- base -}
 import Data.Bits {- base -}
+import System.Environment {- base -}
 import System.Exit {- base -}
 import Text.Printf {- base -}
 
@@ -17,21 +18,24 @@ entry tm x y = printf "%.3f,%.3f,%.3f" tm x y
 has_mask :: Bits a => a -> a -> Bool
 has_mask k m = (m .&. k) == k
 
-trace :: X11 R -> (R,R,Int,Int) -> IO ()
-trace x11 (t0,dt,rx',ry') = do
-  tm <- time
-  (rx,ry,x,y,m) <- x11_ptr_raw x11
-  when (has_mask X.button3Mask m) exitSuccess
-  when (rx /= rx' || ry /= ry') (putStrLn (entry (tm - t0) x y))
-  pauseThread dt
-  trace x11 (t0,dt,rx,ry)
-  return ()
-
-main :: IO ()
-main = do
+-- > trace_rec_x11 0.01
+trace_rec_x11 :: R -> IO ()
+trace_rec_x11 dt = do
   putStrLn "tm,x,y"
   x11 <- x11_init ":0"
   t0 <- time
-  finally (trace x11 (t0,0.001,0,0)) (x11_close x11)
+  let recur (rx',ry') = do
+        tm <- time
+        (rx,ry,x,y,m) <- x11_ptr_raw x11
+        when (has_mask X.button3Mask m) exitSuccess
+        when (rx /= rx' || ry /= ry') (putStrLn (entry (tm - t0) x y))
+        pauseThread dt
+        recur (rx,ry)
+  finally (recur (0,0)) (x11_close x11)
 
-
+main :: IO ()
+main = do
+  a <- getArgs
+  case a of
+    [dt] -> trace_rec_x11 (read dt)
+    _ -> putStrLn "trace-rec-x11 delta-time:float"
