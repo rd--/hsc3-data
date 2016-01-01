65a98bd2e7e9587d7e2289f3e82c9f74ece1bc31
Rohan Drape <rd@slavepianos.org>
2016-01-01 01:08:41 +0000

    ~cmd/csv-plot (+im)

--- cmd/csv-plot.hs
+++ cmd/csv-plot.hs
@@ -19,20 +19,31 @@ csv_load_col = fmap transpose . csv_load_double
 with_csv_col :: ([[Double]] -> IO r) -> FilePath -> IO r
 with_csv_col f fn = csv_load_col fn >>= \c -> f c
 
-csv_plot_1 :: Int -> FilePath -> IO ()
-csv_plot_1 i = with_csv_col (\c -> plotTable1 (c !! i))
+csv_plot_1 :: ([[Double]] -> IO ()) -> Int -> FilePath -> IO ()
+csv_plot_1 f i = with_csv_col (\c -> f [c !! i])
 
 csv_plot_2 :: (Int,Int) -> FilePath -> IO ()
 csv_plot_2 (i,j) = with_csv_col (\c -> plot_p2_ln [zip (c !! i) (c !! j)])
 
-csv_plot_3 :: (Int,Int,Int) -> FilePath -> IO ()
-csv_plot_3 (i,j,k) = with_csv_col (\c -> plot_p3_ln [zip3 (c !! i) (c !! j) (c !! k)])
+csv_plot_3 :: ([[(Double,Double,Double)]] -> IO ()) -> (Int,Int,Int) -> FilePath -> IO ()
+csv_plot_3 f (i,j,k) = with_csv_col (\c -> f [zip3 (c !! i) (c !! j) (c !! k)])
+
+{-
+derive_mode :: String -> String
+derive_mode md =
+    case md of
+      "ln" -> "with lines"
+      "im" -> "with impulses"
+      _ -> error "derive_mode"
+-}
 
 main :: IO ()
 main = do
   a <- getArgs
   case a of
-    ["ln",fn,x] -> csv_plot_1 (read x) fn
+    ["ln",fn,x] -> csv_plot_1 plotTable (read x) fn
+    ["im",fn,x] -> csv_plot_1 plotImpulses (read x) fn
     ["ln",fn,x,y] -> csv_plot_2 (read x,read y) fn
-    ["ln",fn,x,y,z] -> csv_plot_3 (read x,read y,read z) fn
-    _ -> putStrLn "csv-plot ln csv-file x [y z]"
+    ["ln",fn,x,y,z] -> csv_plot_3 plot_p3_ln (read x,read y,read z) fn
+    ["im",fn,x,y,z] -> csv_plot_3 plot_p3_impulses (read x,read y,read z) fn
+    _ -> putStrLn "csv-plot {ln|im} csv-file x [y z]"
