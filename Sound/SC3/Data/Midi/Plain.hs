efd94a851283f982f7cd166f703754b9ebeed7f3
Rohan Drape <rd@slavepianos.org>
2017-06-26 05:30:45 +0000

    ~Sound/SC3/Data/Midi/Plain.hs (+time_sig)

--- Sound/SC3/Data/Midi/Plain.hs
+++ Sound/SC3/Data/Midi/Plain.hs
@@ -7,54 +7,79 @@ import Data.List {- base -}
 import Data.Maybe {- base -}
 
 import qualified Music.Theory.Array.CSV.Midi.MND as T {- hmt -}
+import qualified Music.Theory.List as T {- hmt -}
 import qualified Music.Theory.Math.Convert as T {- hmt -}
 import qualified Music.Theory.Time.Seq as T {- hmt -}
-import qualified Music.Theory.Tuple as T {- hmt -}
 
 import qualified Codec.Midi as M {- HCodecs -}
 
 -- | Time in fractional seconds.
 type Time = Double
 
--- | (midi-note-number,midi-velocity,midi-channel)
+-- | The first value encodes non-note data as negative numbers.
+--
+--   0-127 = (midi-note-number,midi-velocity,midi-channel)
+--   -1 = (-1:time-signature,numerator,denominator)
 type Event = (Int,Int,Int)
 
--- | ((start-time,duration),event)
+-- | ((start-time,duration),note-event)
 type Note = ((Time,Time),Event)
 
 -- | 'T.Wseq' of 'Event'.
 type SEQ = T.Wseq Time Event
 
--- | Translate 'Note' to on & off messages.
-note_to_midi :: Note -> T.T2 (Time,M.Message)
-note_to_midi ((st,dur),(mn,vel,ch)) =
-    let n_on = (st, M.NoteOn ch mn vel)
-        n_off = (st + dur, M.NoteOff ch mn 0)
-    in (n_on,n_off)
+-- | Translate 'Note' to on & off messages or time-signature message.
+note_to_midi :: Note -> [(Time,M.Message)]
+note_to_midi ((st,dur),(msg,d1,d2)) =
+    let n_on = (st, M.NoteOn d2 msg d1)
+        n_off = (st + dur, M.NoteOff d2 msg 0)
+        ts = (st,mk_time_signature (d1,d2))
+    in case msg of
+         -1 -> [ts]
+         _ -> [n_on,n_off]
 
 -- | Add 'M.TrackEnd' message.
 add_track_end :: T.Tseq t M.Message -> T.Tseq t M.Message
 add_track_end tr = tr ++ [(fst (last tr),M.TrackEnd)]
 
--- | Translate tempo value.
+-- | Translate pulses per minute to micro seconds per quarter note.
 --
--- > map mk_tempo [60,120,240] == [10^6,5*10^5,25*10^4]
-mk_tempo :: Int -> Int
-mk_tempo bpm =
+-- > map ppm_to_mspqn [60,120,240] == [10^6,5*10^5,25*10^4]
+ppm_to_mspqn :: Int -> Int
+ppm_to_mspqn ppm =
     let microseconds_per_minute = 60000000
-    in microseconds_per_minute `div` bpm
+    in microseconds_per_minute `div` ppm
+
+-- > mk_tempo_change 60 == M.TempoChange (10^6)
+mk_tempo_change :: Int -> M.Message
+mk_tempo_change = M.TempoChange . ppm_to_mspqn
+
+mk_denominator :: Int -> Int
+mk_denominator d = T.lookup_err d [(1,0),(2,1),(4,2),(8,3),(16,4),(32,5),(64,6)]
+
+-- > mk_time_signature (4,4) == M.TimeSignature 4 2 24 8
+-- > mk_time_signature (3,4)
+mk_time_signature :: (Int,Int) -> M.Message
+mk_time_signature (nn,d) =
+    let dd = mk_denominator d
+        cc = 24 -- midi ticks per pulse
+        bb = 8 -- 1/32 per 1/4
+    in M.TimeSignature nn dd cc bb
+
+write_midi0_opt :: Maybe Int -> Maybe (Int,Int) -> FilePath -> [SEQ] -> IO ()
+write_midi0_opt m_tc m_ts fn sq =
+    let ft = M.SingleTrack
+        tf = M.TicksPerBeat 1024
+        pre = catMaybes [fmap mk_tempo_change m_tc
+                        ,fmap mk_time_signature m_ts]
+        ev = map (concatMap note_to_midi) sq
+        m = map (\x -> (0,x)) pre ++ concat ev
+        t = M.fromRealTime tf . M.fromAbsTime . add_track_end . sortOn fst $ m
+    in M.exportFile fn (M.Midi ft tf [t])
 
 -- | Write Type-0 midi file, tempo is 60, time signature is 4/4.
 write_midi0 :: FilePath -> [SEQ] -> IO ()
-write_midi0 fn sq =
-    let ft = M.SingleTrack
-        tf = M.TicksPerBeat 1024
-        ts = M.TimeSignature 4 2 24 8
-        tc = M.TempoChange (mk_tempo 60)
-        ev = map (concat . map T.t2_to_list . map note_to_midi) sq
-        m = (0,ts) : (0,tc) : concat ev
-        t = M.fromRealTime tf . M.fromAbsTime . add_track_end . sortOn fst $ m
-    in M.exportFile fn (M.Midi ft tf [t])
+write_midi0 = write_midi0_opt (Just 60) (Just (4,4))
 
 track_to_wseq :: [(Time,M.Message)] -> SEQ
 track_to_wseq =
