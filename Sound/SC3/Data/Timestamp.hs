fd80806e069a3b0b30f827207cb7cbb7d171dd8e
Rohan Drape <rd@slavepianos.org>
2017-09-18 10:07:54 +0000

    ~parse_timestamp

--- Sound/SC3/Data/Timestamp.hs
+++ Sound/SC3/Data/Timestamp.hs
@@ -2,36 +2,53 @@ module Sound.SC3.Data.Timestamp where
 
 import qualified Data.Time as T {- time -}
 
+-- | 'T.parseTimeOrError' of 'T.defaultTimeLocale'.
 parse_time_fmt :: String -> String -> T.LocalTime
 parse_time_fmt fmt = T.parseTimeOrError True T.defaultTimeLocale fmt
 
--- | Allow single-digit hour.
+-- | Strict RFC822 format.
 --
 -- > parse_time_fmt rfc822_fmt "Sat, 26 Mar 2016 19:12:51 +1100"
--- > parse_time_fmt rfc822_fmt "Sat, 20 Sep 2008 9:18:14 +1000"
 rfc822_fmt :: String
-rfc822_fmt = "%a, %_d %b %Y %_H:%M:%S %Z"
+rfc822_fmt = "%a, %d %b %Y %H:%M:%S %Z"
 
+-- | Allow single-digit day and hour.
+--
+-- > parse_time_fmt rfc822_fmt_lenient "Mon, 4 Sep 2017 9:07:09 +1000"
+rfc822_fmt_lenient :: String
+rfc822_fmt_lenient = "%a, %_d %b %Y %_H:%M:%S %Z"
+
+-- | Timezone name as prefix to offset.
+--
 -- > parse_time_fmt rfc822_fmt_infix_named_tz "Sat, 12 Dec 09 16:05:55 GMT-0700"
 rfc822_fmt_infix_named_tz :: String
 rfc822_fmt_infix_named_tz = "%a, %_d %b %Y %_H:%M:%S %Z%z"
 
+-- | Timezone name as parenthesised postfix of offset.
+--
 -- > parse_time_fmt rfc822_fmt_postfix_named_tz "Fri, 11 Nov 2016 12:39:09 +0100 (CET)"
 rfc822_fmt_postfix_named_tz :: String
 rfc822_fmt_postfix_named_tz = "%a, %_d %b %Y %_H:%M:%S %z (%Z)"
 
--- > parse_time_fmt rfc822_fmt_no_day "11 Aug 2008 21:29:30 -0700"
-rfc822_fmt_no_day :: String
-rfc822_fmt_no_day = "%_d %b %Y %_H:%M:%S %Z"
+-- | Without day of week.
+--
+-- > parse_time_fmt rfc822_fmt_no_day_of_week "11 Aug 2008 21:29:30 -0700"
+rfc822_fmt_no_day_of_week :: String
+rfc822_fmt_no_day_of_week = "%_d %b %Y %_H:%M:%S %Z"
 
+-- | Without timezone.
+--
 -- > parse_time_fmt rfc822_fmt_no_tz "Tue, 16 Oct 2007 10:21:54"
 rfc822_fmt_no_tz :: String
 rfc822_fmt_no_tz = "%a, %_d %b %Y %_H:%M:%S"
 
--- > parse_time_fmt iso8601_tz "2014-11-18T19:45:44+11:00"
-iso8601_tz :: String
-iso8601_tz = "%FT%H:%M:%S%z"
+-- | ISO8601 with time-zone.
+--
+-- > parse_time_fmt iso8601_fmt_tz "2014-11-18T19:45:44+11:00"
+iso8601_fmt_tz :: String
+iso8601_fmt_tz = "%FT%H:%M:%S%z"
 
+-- | Apply /f/ at /l/ until if provides a value, or 'Nothing' if it never does.
 locate :: (t -> Maybe u) -> [t] -> Maybe u
 locate f l =
     case l of
@@ -40,32 +57,31 @@ locate f l =
                 Just r -> Just r
                 Nothing -> locate f l'
 
+parse_timestamp_fmt_seq :: T.ParseTime t => [String] -> String -> Maybe t
+parse_timestamp_fmt_seq fmt s =
+    let ptm = T.parseTimeM True T.defaultTimeLocale
+        try x = ptm x s
+    in locate try fmt
+
 {- | Somewhat flexible time-stamp parser.
 
 > let ts = ["Sat, 26 Mar 2016 19:12:51 +1100"
+>          ,"Sat, 20 Sep 2008 9:18:14 +1000"
+>          ,"Sat, 12 Dec 09 16:05:55 GMT-0700"
 >          ,"Fri, 11 Nov 2016 12:39:09 +0100 (CET)"
 >          ,"11 Aug 2008 21:29:30 -0700"
->          ,"Sat, 12 Dec 09 16:05:55 GMT-0700"
->          ,"2014-11-18T19:45:44+11:00"
->          ,"Sat, 20 Sep 2008 9:18:14 +1000"
 >          ,"Tue, 16 Oct 2007 10:21:54"
+>          ,"2014-11-18T19:45:44+11:00"
 >          ,"Wed, 22 Feb 2006 09:39:30 +1100 (AUS Eastern Standard Time)"]
-> in map parse_time ts
+> in map parse_timestamp ts
 
 -}
-parse_timestamp :: String -> T.LocalTime
-parse_timestamp s =
-    let fmt = [rfc822_fmt
-              ,rfc822_fmt_infix_named_tz
-              ,rfc822_fmt_postfix_named_tz
-              ,rfc822_fmt_no_day
-              ,rfc822_fmt_no_tz
-              ,iso8601_tz]
-        ptm = T.parseTimeM True T.defaultTimeLocale
-        try x = ptm x s
-    in case locate try fmt of
-         Just t -> t
-         Nothing ->
-             case ptm rfc822_fmt (take 31 s) of
-               Just t -> t
-               _ -> error (show ("parse_time",s))
+parse_timestamp :: String -> Maybe T.LocalTime
+parse_timestamp =
+    parse_timestamp_fmt_seq
+    [rfc822_fmt_lenient
+    ,rfc822_fmt_infix_named_tz
+    ,rfc822_fmt_postfix_named_tz
+    ,rfc822_fmt_no_day_of_week
+    ,rfc822_fmt_no_tz
+    ,iso8601_fmt_tz]
