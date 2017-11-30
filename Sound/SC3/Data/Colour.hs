503b6f2aaf04dd2d2895efdb45fa51e9407283da
Rohan Drape <rd@slavepianos.org>
2017-11-30 23:15:18 +0000

    +Sound/SC3/Data/Colour.hs

---
+++ Sound/SC3/Data/Colour.hs
@@ -0,0 +1,22 @@
+module Sound.SC3.Data.Colour where
+
+import Data.Word {- base -}
+
+import qualified Music.Theory.Array.CSV as T {- hmt -}
+
+type U8 = Word8
+type RGB24 = (U8,U8,U8)
+
+-- | Read (name,red,green,blue) CSV table.
+--
+-- > let fn = "/home/rohan/sw/hsc3-data/data/colour/svg.csv"
+-- > tbl <- clr_read_csv_rgb24_table fn
+-- > lookup "powderblue" tbl == Just (176,224,230)
+clr_read_csv_rgb24_table :: FilePath -> IO [(String,RGB24)]
+clr_read_csv_rgb24_table fn = do
+  tbl <- T.csv_table_read_def id fn
+  let f e =
+        case e of
+          [nm,r,g,b] -> (nm,(read r,read g,read b))
+          _ -> error "clr_read_csv_rgb24_table"
+  return (map f tbl)
