01945b4a3df6c9d263ed39d2503eae2c5d72a30c
Rohan Drape <rd@slavepianos.org>
2018-06-10 00:16:12 +0000

    ~mk/+typ

--- mk/akwf.hs
+++ mk/akwf.hs
@@ -5,7 +5,8 @@ import qualified WWW.Minus.Fetch as WWW {- www-minus -}
 akfw_get_png_grp :: FilePath -> AKWF_GRP -> IO ()
 akfw_get_png_grp dir = mapM_ WWW.url_fetch . akfw_get_png_cmd dir
 
-akwf_get_png_all dir = mapM_ (akfw_get_png dir . akwf_lookup_err . fst) akwf_grp
+akwf_get_png_all :: FilePath -> IO ()
+akwf_get_png_all dir = mapM_ (akfw_get_png_grp dir . akwf_lookup_err . fst) akwf_grp
 
 {-
 
