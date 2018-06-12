6a04f599d7a9c9855f2df907ae925ee306c10093
Rohan Drape <rd@slavepianos.org>
2018-06-12 02:03:25 +0000

    ~arturia

--- Sound/SC3/Data/Midi/Arturia/BS.hs
+++ Sound/SC3/Data/Midi/Arturia/BS.hs
@@ -1,3 +1,4 @@
+-- | Arturia Beatstep Controller utilities.
 module Sound.SC3.Data.Midi.Arturia.BS where
 
 import Data.Word {- base -}
@@ -38,42 +39,90 @@ set_global_midi_channel ch = set_sysex 0x50 0x0B ch
 set_cv_gate_interface_receive_channel :: U8 -> SYSEX
 set_cv_gate_interface_receive_channel ch = set_sysex 0x50 0x0C ch
 
--- | There are three encoder acceleration modes.
+-- | Encoder acceleration modes.
 --
 -- > map toEnum [0,1,2] == [Slow,Medium,Fast]
 data Acceleration = Slow | Medium | Fast deriving (Eq,Enum,Show)
 
--- 0=slow, 1=medium, 2=fast
 set_encoder_acceleration :: U8 -> SYSEX
 set_encoder_acceleration x = set_sysex 0x41 0x04 x
 
+-- | Pad velocity curves.
+--
 -- > map toEnum [0 .. 3] == [Linear,Logarithmic,Exponential,Full]
 data Curve = Linear | Logarithmic | Exponential | Full deriving (Eq,Enum,Show)
 
--- 0=linear, 1=logarithmic, 2=exponential, 3=full
 pad_velocity_curve :: U8 -> SYSEX
 pad_velocity_curve x = set_sysex 0x41 0x03 x
 
--- * ENC = encoder
+store_preset :: U8 -> SYSEX
+store_preset mm = [0xF0,0x00,0x20,0x6B,0x7F,0x42,0x06,mm,0xF7]
 
-enc_cm_set_cc_sysex :: U8 -> U8 -> SYSEX
-enc_cm_set_cc_sysex k cc = set_sysex 0x03 k cc
+recall_preset :: U8 -> SYSEX
+recall_preset mm = [0xF0,0x00,0x20,0x6B,0x7F,0x42,0x05,mm,0xF7]
 
-enc_cm_set_ch_sysex :: U8 -> U8 -> SYSEX
-enc_cm_set_ch_sysex k ch = set_sysex 0x02 k ch
+-- * ENC = encoder, cm = CC-mode
 
-enc_cm_set_abs_sysex :: U8 -> (U8,U8,(U8,U8)) -> [SYSEX]
-enc_cm_set_abs_sysex k (ch,cc,(l,r)) =
-  [set_sysex 0x01 k 0x01 -- control-change mode
+{- | Encoder control-change mode.
+
+k = control-id (ie. 0x20-0x2F),
+ch = channel, cc = control-change-number,
+l = left, r = right,
+md = mode
+-}
+enc_cm_set :: U8 -> (U8,U8,(U8,U8),U8) -> [SYSEX]
+enc_cm_set k (ch,cc,(l,r),md) =
+  [set_sysex 0x01 k 0x01
   ,set_sysex 0x02 k ch
   ,set_sysex 0x03 k cc
   ,set_sysex 0x04 k l
-  ,set_sysex 0x05 k r
-  ,set_sysex 0x06 k 0x00 -- absolute-value mode
+  ,set_sysex 0x05 k r -- absolute mode only
+  ,set_sysex 0x05 k r -- absolute mode only
+  ,set_sysex 0x06 k md -- 0=absolute-value 1-3=relative
   ]
 
-enc_cm_set_basic_sysex :: U8 -> U8 -> [SYSEX]
-enc_cm_set_basic_sysex ch cc0 = concatMap (\k -> enc_cm_set_abs_sysex (0x20 + k) (ch,cc0 + k,(0,127))) [0 .. 15]
+enc_cm_set_16 :: (U8,U8,(U8,U8),U8) -> [SYSEX]
+enc_cm_set_16 (ch,cc0,rng,md) =
+  let f k = enc_cm_set (0x20 + k) (ch,cc0 + k,rng,md)
+  in concatMap f [0 .. 15]
+
+enc_cm_set_cc :: U8 -> U8 -> SYSEX
+enc_cm_set_cc k cc = set_sysex 0x03 k cc
+
+enc_cm_set_ch :: U8 -> U8 -> SYSEX
+enc_cm_set_ch k ch = set_sysex 0x02 k ch
+
+-- * PAD, nm = note-mode
+
+data Switch_Mode = Toggle | Gate deriving (Eq,Enum,Show)
+
+pad_nm_set :: U8 -> (U8, U8, U8) -> [SYSEX]
+pad_nm_set k (ch,mnn,md) =
+  [set_sysex 0x01 k 0x09 -- midi-note-number mode
+  ,set_sysex 0x02 k ch
+  ,set_sysex 0x03 k mnn
+  ,set_sysex 0x06 k md -- 0=toggle,1=gate
+  ]
+
+pad_nm_set_16 :: (U8, U8, U8) -> [SYSEX]
+pad_nm_set_16 (ch,mnn0,md) =
+  let f k = pad_nm_set (0x70 + k) (ch,mnn0 + k,md)
+  in concatMap f [0 .. 15]
+
+pad_cm_set :: U8 -> (U8, U8, (U8, U8), U8) -> [SYSEX]
+pad_cm_set k (ch,cc,(off,on),md) =
+  [set_sysex 0x01 k 0x08 -- control-change mode
+  ,set_sysex 0x02 k ch
+  ,set_sysex 0x03 k cc
+  ,set_sysex 0x04 k off
+  ,set_sysex 0x05 k on
+  ,set_sysex 0x06 k md -- 0=toggle,1=gate
+  ]
+
+pad_cm_set_16 :: (U8, U8, (U8, U8), U8) -> [SYSEX]
+pad_cm_set_16 (ch,cc0,off_on,md) =
+  let f k = pad_cm_set (0x70 + k) (ch,cc0 + k,off_on,md)
+  in concatMap f [0 .. 15]
 
 -- * DSC = description
 
@@ -99,8 +148,8 @@ pad_nm_param_dsc =
   ,(0x03,"MNN")
   ,(0x06,"BEHAVIOUR")]
 
-pad_nm_behaviour_dsc :: [(U8,String)]
-pad_nm_behaviour_dsc =
+pad_behaviour_dsc :: [(U8,String)]
+pad_behaviour_dsc =
   [(0x00,"TOGGLE")
   ,(0x01,"GATE")]
 
@@ -122,8 +171,8 @@ enc_cm_param_dsc =
   ,(0x05,"MAXIMA")
   ,(0x06,"BEHAVIOUR")]
 
-enc_cm_behaviour_dsc :: [(U8,String)]
-enc_cm_behaviour_dsc =
+enc_behaviour_dsc :: [(U8,String)]
+enc_behaviour_dsc =
   [(0x00,"ABSOLUTE")
   ,(0x01,"RELATIVE_1")
   ,(0x02,"RELATIVE_2")
@@ -154,13 +203,18 @@ run = PM.pm_with_output_device k
 
 run (\fd -> PM.pm_sysex_write fd (set_encoder_acceleration (enum_to_u8 Slow)))
 
-run (\fd -> PM.pm_sysex_write fd (enc_cm_set_ch_sysex 0x20 0x00))
-run (\fd -> PM.pm_sysex_write fd (enc_cm_set_cc_sysex 0x20 0x00))
+run (\fd -> PM.pm_sysex_write fd (enc_cm_set_ch 0x20 0x00))
+run (\fd -> PM.pm_sysex_write fd (enc_cm_set_cc 0x20 0x00))
 
-run (\fd -> PM.pm_sysex_write_set fd (enc_cm_set_abs_sysex 0x20 (0x00,0x00,(0x00,0x7F))))
-run (\fd -> PM.pm_sysex_write_set fd (enc_cm_set_basic_sysex 0 0))
+run (\fd -> PM.pm_sysex_write_set fd (enc_cm_set 0x20 (0x00,0x00,(0x00,0x7F),0x01)))
 
-PM.pm_with_output_device k (\fd -> PM.pm_cvm3_write fd (0x90,0x2C,0x7F))
-PM.pm_with_output_device k (\fd -> PM.pm_cvm3_write fd (0x90,0x2C,0x00))
+run (\fd -> PM.pm_sysex_write_set fd (enc_cm_set_16 (0,0,(0,127),0x01)))
+
+run (\fd -> PM.pm_sysex_write_set fd (pad_nm_set_16 (0,0,enum_to_u8 Toggle)))
+
+run (\fd -> PM.pm_sysex_write_set fd (pad_cm_set_16 (0,0,(0,127),enum_to_u8 Toggle)))
+
+PM.pm_with_output_device k (\fd -> PM.pm_cvm3_write fd (0x90,0x00,0x7F))
+PM.pm_with_output_device k (\fd -> PM.pm_cvm3_write fd (0x90,0x00,0x00))
 
 -}
