556d6ee437ae91d7ef44230d9138f62797a4229e
Rohan Drape <rd@slavepianos.org>
2018-08-28 05:38:31 +0000

    ~svl/->hmt

--- Sound/SC3/Data/XML/SVL.hs
+++ Sound/SC3/Data/XML/SVL.hs
@@ -2,6 +2,7 @@
 module Sound.SC3.Data.XML.SVL where
 
 import Data.List {- base -}
+import Data.Maybe {- base -}
 
 import qualified Data.ByteString as B {- bytestring -}
 import qualified Text.XML.Light as X {- xml -}
@@ -55,9 +56,10 @@ svl_get_dataset = XML.x_get_elem_path ["data","dataset"]
 
 -- | Load the /sv/ element from an SVL file.
 svl_load :: FilePath -> IO (Maybe X.Element)
-svl_load fn = do
-  b <- B.readFile fn
-  return (X.parseXMLDoc b)
+svl_load = fmap X.parseXMLDoc . B.readFile
+
+svl_load_err :: FilePath -> IO X.Element
+svl_load_err = fmap (fromMaybe (error "svl_load")) . svl_load
 
 -- * SN = SPARSE-NOTE
 
@@ -85,17 +87,17 @@ svl_parse_point e =
      ,read (XML.x_get_attr "level" e)
      ,XML.x_get_attr "label" e))
 
-svl_load_sparse_note :: FilePath -> IO (SR,T.Wseq FRAME SVL_SN_PT_DATA)
-svl_load_sparse_note fn = do
-  Just e <- svl_load fn
+svl_parse_sparse_note :: X.Element -> (SR, T.Wseq FRAME SVL_SN_PT_DATA)
+svl_parse_sparse_note e =
   let md = svl_get_model_of_type ("sparse","note") e
       sr = svl_model_sample_rate md
       pt = XML.x_get_elem_set "point" (svl_get_dataset e)
-  return (sr,map svl_parse_point pt)
+  in (sr,map svl_parse_point pt)
+
+svl_load_sparse_note :: FilePath -> IO (SR,T.Wseq FRAME SVL_SN_PT_DATA)
+svl_load_sparse_note = fmap svl_parse_sparse_note . svl_load_err
 
 -- * CSEC/SEC
-
-type CSEC = Int
 
 -- | Quantise to nearest multiple of /k/.
 --
@@ -110,7 +112,7 @@ quantise k n =
 -- | q = quantise, sr = sample-rate, x = frame-count
 --
 -- > svl_frame_to_csec 50 48000 72000 == 150
-svl_frame_to_csec :: Int -> SR -> Int -> CSEC
+svl_frame_to_csec :: Int -> SR -> Int -> T.CSEC
 svl_frame_to_csec q sr x = quantise q (round (100 * fromIntegral x / sr))
 
 svl_load_sparse_note_tm :: (SR -> FRAME -> t) -> FilePath -> IO (T.Wseq t SVL_SN_PT_DATA)
@@ -119,30 +121,23 @@ svl_load_sparse_note_tm tm_f fn = do
   let bimap1 f (t,u) = (f t,f u)
   return (T.wseq_tmap (bimap1 (tm_f sr)) sq)
 
-svl_load_sparse_note_csec :: Int -> FilePath -> IO (T.Wseq CSEC SVL_SN_PT_DATA)
+svl_load_sparse_note_csec :: Int -> FilePath -> IO (T.Wseq T.CSEC SVL_SN_PT_DATA)
 svl_load_sparse_note_csec q = svl_load_sparse_note_tm (svl_frame_to_csec q)
 
-type SEC = Int
-
-svl_frame_to_sec :: SR -> Int -> SEC
+svl_frame_to_sec :: SR -> Int -> T.SEC
 svl_frame_to_sec sr x = round (fromIntegral x / sr)
 
-svl_load_sparse_note_sec :: FilePath -> IO (T.Wseq SEC SVL_SN_PT_DATA)
+svl_load_sparse_note_sec :: FilePath -> IO (T.Wseq T.SEC SVL_SN_PT_DATA)
 svl_load_sparse_note_sec = svl_load_sparse_note_tm svl_frame_to_sec
 
 svl_load_sparse_note_mnn_accum :: (Ord t,Num t) => (SR -> FRAME -> t) -> FilePath -> IO (Bool,T.Tseq t ([Int], [Int], [Int]))
-svl_load_sparse_note_mnn_accum tm_f fn = do
-  sq <- svl_load_sparse_note_tm tm_f fn
-  let sel_mnn (mnn,_,_) = mnn
-      mnn_sq = T.wseq_map sel_mnn sq
-      ol = T.wseq_has_overlaps (==) mnn_sq
-      mnn_sq_edit = if ol then T.wseq_remove_overlaps (==) id mnn_sq else mnn_sq
-      a_sq = T.tseq_begin_end_accum (T.tseq_group (T.wseq_begin_end mnn_sq_edit))
-  return (ol,a_sq)
+svl_load_sparse_note_mnn_accum tm_f =
+  fmap (T.wseq_begin_end_accum . T.wseq_map (\(e,_,_) -> e)) .
+  svl_load_sparse_note_tm tm_f
 
 -- * SVL_NODE
 
--- | (start-time,([note],[dur]))
+-- | (start-time,([note],[duration]))
 type SVL_NODE t n = (t,([n],[t]))
 
 svl_node_map :: ([u] -> [v]) -> SVL_NODE t u -> SVL_NODE t v
@@ -168,7 +163,7 @@ svl_load_node_p tm_f mnn_f =
   fmap (map (svl_node_map T.spell_midi_set)) .
   svl_load_node_m tm_f mnn_f
 
-svl_node_p_csec_pp :: SVL_NODE_p CSEC -> String
+svl_node_p_csec_pp :: SVL_NODE_p T.CSEC -> String
 svl_node_p_csec_pp (tm,(p,du)) =
   let csec_tm_pp = T.mincsec_pp_opt True . T.csec_to_mincsec
       csec_du_pp = show . flip div 100
@@ -176,5 +171,5 @@ svl_node_p_csec_pp (tm,(p,du)) =
              ,intercalate "," (map T.pitch_pp p)
              ,intercalate "," (map csec_du_pp du)]
 
-svl_node_p_csec_seq_wr :: [SVL_NODE_p CSEC] -> IO ()
+svl_node_p_csec_seq_wr :: [SVL_NODE_p T.CSEC] -> IO ()
 svl_node_p_csec_seq_wr =  putStrLn . unlines . map svl_node_p_csec_pp
