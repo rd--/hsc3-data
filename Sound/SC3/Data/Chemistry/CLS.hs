512260143da91818068cbed6b6faa3014acdf55a
Rohan Drape <rd@slavepianos.org>
2018-11-01 10:47:44 +0000

    ~Chemistry/CLS

--- Sound/SC3/Data/Chemistry/CLS.hs
+++ Sound/SC3/Data/Chemistry/CLS.hs
@@ -7,20 +7,23 @@ Center for Computational Materials Science
 -}
 module Sound.SC3.Data.Chemistry.CLS where
 
+import System.Environment {- base -}
 import System.FilePath {- filepath -}
 
 import Sound.SC3.Data.Chemistry.STRUCT {- hsc3-data -}
 import qualified Sound.SC3.Data.Chemistry.XYZ as XYZ {- hsc3-data -}
 
-cls_dir :: FilePath
-cls_dir = "/home/rohan/sw/hsc3-data/data/chemistry/cls/"
-
-cls_file :: FilePath -> FilePath
-cls_file = (++) cls_dir
+-- | Lookup CLS_DIR, or default to local directory.
+cls_dir :: IO FilePath
+cls_dir = do
+  r <-  lookupEnv "CLS_DIR"
+  return (case r of
+            Just dir -> dir
+            Nothing -> "/home/rohan/sw/hsc3-data/data/chemistry/cls/")
 
 -- | Make CLS xyz filename from ID.
 cls_xyz_file :: String -> FilePath
-cls_xyz_file nm = cls_file ("xyz" </> nm <.> "xyz")
+cls_xyz_file nm = "xyz" </> nm <.> "xyz"
 
 -- | Load a CLS XYZ file as a STRUCT.
 --
@@ -28,14 +31,16 @@ cls_xyz_file nm = cls_file ("xyz" </> nm <.> "xyz")
 -- > struct_bounds s
 cls_load :: String -> IO STRUCT
 cls_load nm = do
-  x <- XYZ.xyz_load (cls_xyz_file nm)
+  d <- cls_dir
+  x <- XYZ.xyz_load (d </> cls_xyz_file nm)
   return (xyz_to_struct (nm,x))
 
--- | Load a all CLS XYZ files as STRUCTs.
+-- | Load all CLS XYZ files as STRUCTs.
 --
 -- > sq <- cls_load_dir
 -- > length sq == 268
 cls_load_dir :: IO [STRUCT]
 cls_load_dir = do
-  s <- XYZ.xyz_load_dir (cls_file "xyz")
+  d <- cls_dir
+  s <- XYZ.xyz_load_dir (d </> "xyz")
   return (map xyz_to_struct s)
