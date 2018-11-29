888bf1200d79a62f8af03dca26b6c51905a6b465
Rohan Drape <rd@slavepianos.org>
2018-11-29 10:54:45 +0000

    +qual.

--- Sound/SC3/Data/Graph/Dot.hs
+++ Sound/SC3/Data/Graph/Dot.hs
@@ -7,8 +7,8 @@ import System.Process {- process -}
 import qualified Data.Text.Lazy as T {- text -}
 import qualified Data.Text.Lazy.IO as T {- text -}
 
-import Data.GraphViz {- graphviz -}
-import Data.GraphViz.Attributes.Complete {- graphviz -}
+import qualified Data.GraphViz as GV {- graphviz -}
+import qualified Data.GraphViz.Attributes.Complete as GV {- graphviz -}
 import qualified Data.GraphViz.Types.Generalised as G {- graphviz -}
 
 -- | Run dot to insert layout information into graph.
@@ -20,52 +20,52 @@ import qualified Data.GraphViz.Types.Generalised as G {- graphviz -}
 dot_run_layout :: String -> IO String
 dot_run_layout = readProcess "dot" ["-T","dot"]
 
--- | 'parseDotGraph' of 'T.pack'.
-dg_parse :: (Ord t,ParseDot t) => String -> G.DotGraph t
-dg_parse = parseDotGraph . T.pack
+-- | 'parseGV.DotGraph' of 'T.pack'.
+dg_parse :: (Ord t,GV.ParseDot t) => String -> G.DotGraph t
+dg_parse = GV.parseDotGraph . T.pack
 
 -- | Type specialised.
 dg_parse_int :: String -> G.DotGraph Int
-dg_parse_int = parseDotGraph . T.pack
+dg_parse_int = GV.parseDotGraph . T.pack
 
--- | 'T.unpack' of 'printDotGraph'.
-dg_print :: (Ord t,PrintDot t) => G.DotGraph t -> String
-dg_print = T.unpack . printDotGraph
+-- | 'T.unpack' of 'printGV.DotGraph'.
+dg_print :: (Ord t,GV.PrintDot t) => G.DotGraph t -> String
+dg_print = T.unpack . GV.printDotGraph
 
--- | 'parseDotGraph' of 'T.readFile'.
-dg_load :: (Ord t,ParseDot t) => FilePath -> IO (G.DotGraph t)
-dg_load = fmap parseDotGraph . T.readFile
+-- | 'parseGV.DotGraph' of 'T.readFile'.
+dg_load :: (Ord t,GV.ParseDot t) => FilePath -> IO (G.DotGraph t)
+dg_load = fmap GV.parseDotGraph . T.readFile
 
 -- | Type specialised.
 dg_load_int :: FilePath -> IO (G.DotGraph Int)
 dg_load_int = dg_load
 
--- | 'DotStatement' to 'DotNode'.
-ds_to_dn :: G.DotStatement n -> Maybe (DotNode n)
+-- | 'GV.DotStatement' to 'GV.DotNode'.
+ds_to_dn :: G.DotStatement n -> Maybe (GV.DotNode n)
 ds_to_dn st =
   case st of
     G.DN n -> Just n
     _ -> Nothing
 
--- | 'DotStatement' to 'DotEdge'.
-ds_to_de :: G.DotStatement n -> Maybe (DotEdge n)
+-- | 'GV.DotStatement' to 'GV.DotEdge'.
+ds_to_de :: G.DotStatement n -> Maybe (GV.DotEdge n)
 ds_to_de st =
   case st of
     G.DE e -> Just e
     _ -> Nothing
 
 -- | 'Attribute' to position.
-attr_to_pos :: Attribute -> Maybe (Double, Double)
+attr_to_pos :: GV.Attribute -> Maybe (Double, Double)
 attr_to_pos a =
   case a of
-    Pos (PointPos (Point x y _ _)) -> Just (x,y)
+    GV.Pos (GV.PointPos (GV.Point x y _ _)) -> Just (x,y)
     _ -> Nothing
 
 -- | 'Attribute' to label.
-attr_to_label :: Attribute -> Maybe String
+attr_to_label :: GV.Attribute -> Maybe String
 attr_to_label a =
   case a of
-    Label (StrLabel t) -> Just (T.unpack t)
+    GV.Label (GV.StrLabel t) -> Just (T.unpack t)
     _ -> Nothing
 
 -- | Vertex.
@@ -83,8 +83,8 @@ type GR_pos t = ([V_pos t], [E t])
 -- | 'V' with label and position.
 type V_lbl_pos t = (t, (String, (Double, Double)))
 
--- | 'DotNode' to 'V_lbl_pos'.
-dn_to_v_lbl_pos :: DotNode t -> V_lbl_pos t
+-- | 'GV.DotNode' to 'V_lbl_pos'.
+dn_to_v_lbl_pos :: GV.DotNode t -> V_lbl_pos t
 dn_to_v_lbl_pos n =
   let a = G.nodeAttributes n
   in case (mapMaybe attr_to_label a,mapMaybe attr_to_pos a) of
@@ -92,11 +92,11 @@ dn_to_v_lbl_pos n =
        ([l],[p]) -> (G.nodeID n,(l,p))
        _ -> error "dn_parse"
 
--- | 'DotEdge' to 'E'.
-de_to_e :: DotEdge t -> E t
+-- | 'GV.DotEdge' to 'E'.
+de_to_e :: GV.DotEdge t -> E t
 de_to_e e = (G.fromNode e,G.toNode e)
 
--- | 'DotGraph' to 'V_lbl_pos' graph.
+-- | 'GV.DotGraph' to 'V_lbl_pos' graph.
 dg_to_gr_lbl_pos  :: G.DotGraph t -> ([V_lbl_pos t], [E t])
 dg_to_gr_lbl_pos g =
   let st = F.toList (G.graphStatements g)
@@ -104,7 +104,7 @@ dg_to_gr_lbl_pos g =
       e = mapMaybe ds_to_de st
   in (map dn_to_v_lbl_pos n,map de_to_e e)
 
--- | 'DotGraph' to 'V_pos' graph.
+-- | 'GV.DotGraph' to 'V_pos' graph.
 dg_to_gr_pos :: G.DotGraph t -> GR_pos t
 dg_to_gr_pos g =
   let (n,e) = dg_to_gr_lbl_pos g
@@ -118,6 +118,7 @@ gr_pos_bounds (v,_) =
       bimap f (i,j) = (f i,f j)
   in (bimap minimum r,bimap maximum r)
 
+-- | Scale graph position data by /mul/.
 gr_pos_scale :: Double -> GR_pos t -> GR_pos t
 gr_pos_scale mul (v,e) =
   let bimap f (i,j) = (f i,f j)
