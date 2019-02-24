b04a83ce485cd3a9361a5ea8858d9516aeb089b4
rd <rd@slavepianos.org>
2019-02-24 02:22:23 +0000

    ~dx7

--- cmd/dx7.hs
+++ cmd/dx7.hs
@@ -5,29 +5,32 @@ import qualified Sound.SC3.Data.Yamaha.DX7 as DX7 {- hsc3-data -}
 
 usage :: IO ()
 usage =
-    let h = ["hex|sysex print parameters file-name"
-            ,"hex|sysex print voice-names file-name"
-            ,"hex|sysex print voice-data-list file-name"
-            ,"sysex verify file-name"
+    let h = ["hex|sysex print parameters file-name..."
+            ,"hex|sysex print voice-names file-name..."
+            ,"hex|sysex print voice-data-list file-name..."
+            ,"sysex verify file-name..."
             ,"sysex rewrite input-file output-file"]
     in putStrLn (unlines h)
 
-dx7_hex_print :: FilePath -> (DX7.DX7_Bank -> [String]) -> IO ()
-dx7_hex_print fn op =
+dx7_hex_print :: (DX7.DX7_Bank -> [String]) -> [FilePath] -> IO ()
+dx7_hex_print op =
   let wr = putStrLn . unlines
-  in DX7.dx7_load_sysex_hex fn >>= wr . op
+  in mapM_ (\fn -> DX7.dx7_load_sysex_hex fn >>= wr . op)
 
-dx7_sysex_print :: FilePath -> (DX7.DX7_Bank -> [String]) -> IO ()
-dx7_sysex_print fn op =
+dx7_sysex_print :: (DX7.DX7_Bank -> [String]) -> [FilePath] -> IO ()
+dx7_sysex_print op =
   let wr = putStrLn . unlines
-  in DX7.dx7_load_sysex fn >>= wr . op
+  in mapM_ (\fn -> DX7.dx7_load_sysex fn >>= wr . op)
 
-dx7_sysex_verify :: FilePath -> IO ()
-dx7_sysex_verify fn = do
+dx7_sysex_verify_1 :: FilePath -> IO ()
+dx7_sysex_verify_1 fn = do
   dat <- DX7.dx7_load_sysex_u8 fn
   bnk <- DX7.dx7_load_sysex fn
   let r = DX7.dx7_sysex_u8_verify dat == (True,True,True,True) && DX7.dx7_bank_verify bnk
   putStrLn (if r then "TRUE" else "FALSE: " ++ show (dat,bnk))
+
+dx7_sysex_verify :: [FilePath] -> IO ()
+dx7_sysex_verify = mapM_ dx7_sysex_verify_1
 
 dx7_sysex_rewrite :: FilePath -> FilePath -> IO ()
 dx7_sysex_rewrite fn1 fn2 = do
@@ -44,12 +47,12 @@ main = do
       print_voice_names = map DX7.dx7_voice_name
       print_voice_data_list = ic . map DX7.dx7_voice_data_list_pp
   case a of
-    ["hex","print","parameters",fn] -> dx7_hex_print fn print_parameters
-    ["hex","print","voice-names",fn] -> dx7_hex_print fn print_voice_names
-    ["hex","print","voice-data-list",fn] -> dx7_hex_print fn print_voice_data_list
-    ["sysex","print","parameters",fn] -> dx7_sysex_print fn print_parameters
-    ["sysex","print","voice-names",fn] -> dx7_sysex_print fn print_voice_names
-    ["sysex","print","voice-data-list",fn] -> dx7_sysex_print fn print_voice_data_list
-    ["sysex","verify",fn] -> dx7_sysex_verify fn
+    "hex":"print":"parameters":fn -> dx7_hex_print print_parameters fn
+    "hex":"print":"voice-names":fn -> dx7_hex_print print_voice_names fn
+    "hex":"print":"voice-data-list":fn -> dx7_hex_print print_voice_data_list fn
+    "sysex":"print":"parameters":fn -> dx7_sysex_print print_parameters fn
+    "sysex":"print":"voice-names":fn -> dx7_sysex_print print_voice_names fn
+    "sysex":"print":"voice-data-list":fn -> dx7_sysex_print print_voice_data_list fn
+    "sysex":"verify":fn -> dx7_sysex_verify fn
     ["sysex","rewrite",fn1,fn2] -> dx7_sysex_rewrite fn1 fn2
     _ -> usage
