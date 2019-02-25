9142ffd86704748b69626e5ab5838fefd14b15a5
rd <rd@slavepianos.org>
2019-02-25 04:06:45 +0000

    +data/yamaha/dx7/vrc

--- Sound/SC3/Data/Yamaha/DX7.hs
+++ Sound/SC3/Data/Yamaha/DX7.hs
@@ -654,6 +654,27 @@ dx7_rom_tbl =
 dx7_rom_syx_name :: (Int,Char,String) -> String
 dx7_rom_syx_name (p,q,_) = "ROM" ++ show p ++ [q]
 
+-- | DX7 VRC-Series cartridges
+dx7_vrc_tbl :: [(Int,String)]
+dx7_vrc_tbl =
+  [(101,"Keyboard, Plucked & Tuned Percussion Group")
+  ,(102,"Wind Instrument Group")
+  ,(103,"Sustain Group")
+  ,(104,"Percussion Group")
+  ,(105,"Sound Effect Group")
+  ,(106,"Synthesizer Group")
+  ,(107,"Special Selection - David Bristow")
+  ,(108,"Special Selection - Gary Leuenberger")
+  ,(109,"Studio 64")
+  ,(110,"Special Selection - Bo Tomlyn")
+  ,(111,"Special Selection - Bo Tomlyn II")
+  ,(112,"Live 64")]
+
+-- | DX7 VRC cartridge to SYSEX file name.
+--
+-- > concatMap dx7_vrc_syx_name dx7_vrc_tbl
+dx7_vrc_syx_name :: (Int,String) -> [String]
+dx7_vrc_syx_name (p,_) = map (\c -> "VRC-" ++ show p ++ ['-',c]) "AB"
 
 -- * E: Function Parameters: (GROUP=2)
 
