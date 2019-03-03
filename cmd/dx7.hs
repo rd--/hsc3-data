5ef79ff375be545fd3b7acf8693fe321d2c23f30
rd <rd@slavepianos.org>
2019-03-03 22:28:10 +0000

    ~cmd/dx7,track

--- cmd/dx7.hs
+++ cmd/dx7.hs
@@ -16,9 +16,9 @@ dx7_sysex_print op =
 
 dx7_sysex_verify_1 :: FilePath -> IO ()
 dx7_sysex_verify_1 fn = do
-  dat <- DX7.dx7_load_sysex_u8 fn
-  bnk <- DX7.dx7_load_sysex fn
-  let r = DX7.dx7_sysex_u8_verify dat == (True,True,True,True) && DX7.dx7_bank_verify bnk
+  dat <- DX7.dx7_read_fmt9_sysex fn
+  bnk <- DX7.dx7_load_fmt9_sysex fn
+  let r = DX7.dx7_fmt9_sysex_verify 0 dat == (True,True,True,True) && DX7.dx7_bank_verify bnk
   putStrLn (if r then "TRUE" else "FALSE: " ++ show (dat,bnk))
 
 dx7_sysex_verify :: [FilePath] -> IO ()
@@ -26,16 +26,16 @@ dx7_sysex_verify = mapM_ dx7_sysex_verify_1
 
 dx7_sysex_add :: FilePath -> FilePath -> IO ()
 dx7_sysex_add fn1 fn2 = do
-  dat <- DX7.dx7_load_u8 fn1
+  dat <- DX7.dx7_read_u8 fn1
   when (length dat /= 4096) (error "dx7_sysex_add: NOT 4096")
-  DX7.dx7_store_sysex_u8 fn2 (DX7.dx7_sysex_fmt_9_gen dat)
+  DX7.dx7_write_fmt9_sysex fn2 (DX7.dx7_fmt9_sysex_gen 0 dat)
 
 dx7_sysex_rewrite :: FilePath -> FilePath -> IO ()
 dx7_sysex_rewrite fn1 fn2 = do
-  src <- DX7.dx7_load_u8 fn1 -- ie. do not verify
-  let dat = DX7.dx7_sysex_fmt_9_dat src
+  src <- DX7.dx7_read_u8 fn1 -- ie. do not verify
+  let dat = DX7.dx7_fmt9_sysex_dat src
   when (length dat /= 4096) (error "dx7_sysex_rewrite?")
-  DX7.dx7_store_sysex_u8 fn2 (DX7.dx7_sysex_fmt_9_gen dat)
+  DX7.dx7_write_fmt9_sysex fn2 (DX7.dx7_fmt9_sysex_gen 0 dat)
 
 usage :: IO ()
 usage =
