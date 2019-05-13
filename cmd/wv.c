3073824176c51190d32a08013fef239843624c20
rd <rd@slavepianos.org>
2019-05-13 02:58:36 +0000

    +md/wv.md

--- cmd/wv.c
+++ cmd/wv.c
@@ -1,9 +1,6 @@
-/* gcc -i ~/opt/src/webview wv.c -DWEBVIEW_GTK=1 `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o wv */
-
 #define WEBVIEW_IMPLEMENTATION
 #include "webview.h"
 
-/* ./wv file:///tmp/t.html 500 300 false */
 int main(int argc, char *argv[]) {
     if(argc == 5) {
         webview("wv",argv[1], atoi(argv[2]), atoi(argv[3]), argv[4][0] == 't');
---
+++ md/wv.md
@@ -0,0 +1,11 @@
+# wv
+
+GTK3/WEBKIT2 minimal viewer.
+
+~~~~
+$ hsc3-wv -h
+wv uri width height resizeable
+$ hsc3-wv file:///tmp/t.html 500 300 false
+...
+$
+~~~~
