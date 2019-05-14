ea894e9a54bf68122b4d4a9d86a88861ea4da9cc
rd <rd@slavepianos.org>
2019-05-14 12:11:11 +0000

    ~wv,case

--- cmd/wv.c
+++ cmd/wv.c
@@ -2,11 +2,12 @@
 
 #define WEBVIEW_IMPLEMENTATION
 #define WEBVIEW_GTK
+#include <ctype.h>
 #include "webview.h"
 
 int main(int argc, char *argv[]) {
     if(argc == 5) {
-        webview("wv",argv[1], atoi(argv[2]), atoi(argv[3]), argv[4][0] == 't');
+        webview("wv",argv[1], atoi(argv[2]), atoi(argv[3]), tolower(argv[4][0]) == 't');
         return 0;
     } else {
         fprintf(stderr,"wv uri width height resizeable\n");
