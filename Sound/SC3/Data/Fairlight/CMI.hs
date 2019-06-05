8ed091069521208fbc7a19bda208405f7e4bcb85
rd <rd@slavepianos.org>
2019-06-05 12:26:36 +0000

    ~cmi,+vol/mnn

--- Sound/SC3/Data/Fairlight/CMI.hs
+++ Sound/SC3/Data/Fairlight/CMI.hs
@@ -18,22 +18,25 @@ import Sound.File.HSndFile {- hsc3-sf-sndfile -}
 
 import Sound.SC3.Data.SFZ {- hsc3-data -}
 
--- | (LOOP-MODE,LOOP-START,LOOP-END,EG-ATTACK,EG-RELEASE)
+-- | (VOLUME,KEY-CENTER,LOOP-MODE,LOOP-START,LOOP-END,EG-ATTACK,EG-RELEASE)
 --
--- In all cases VOLUME=-3 ; KEYCENTER=C4
-type CMI_SFZ = (Char, Int, Int, Double, Double)
+-- For CMI in all cases VOLUME=-3 ; KEYCENTER=C4=60
+type CMI_SFZ = (Double, Int, Char, Int, Int, Double, Double)
 
 -- | Parse SFZ <region>.
 cmi_sfz_rgn_parse :: SFZ_Region -> CMI_SFZ
 cmi_sfz_rgn_parse r =
   let vol = sfz_region_volume r
       mnn = sfz_region_pitch_keycenter r
-      err = error "cmi_sfz_rgn_parse?"
-      lp_mode = fromMaybe err (sfz_region_loop_mode_sym r)
-  in if vol /= -3 || mnn /= 60
-     then err
-     else (lp_mode,sfz_region_loop_start r,sfz_region_loop_end r
-          ,sfz_region_ampeg_attack r,sfz_region_ampeg_release r)
+      lp_mode = fromMaybe (error "cmi_sfz_rgn_parse?") (sfz_region_loop_mode_sym r)
+  in (vol,mnn,lp_mode,sfz_region_loop_start r,sfz_region_loop_end r
+     ,sfz_region_ampeg_attack r,sfz_region_ampeg_release r)
+
+cmi_sfz_verify :: CMI_SFZ -> CMI_SFZ
+cmi_sfz_verify r@(vol,mnn,_,_,_,_,_) =
+  if vol /= -3 || mnn /= 60
+  then error "cmi_sfz_verify?"
+  else r
 
 -- | SFZ and SND data.
 type CMI_DAT = ([SFZ_Region],SF_Header)
@@ -58,7 +61,7 @@ cmi_load_sfz ext dir = fmap cmi_sfz_parse . cmi_load_dat ext dir
 
 -- | Pretty-printer.
 cmi_sfz_pp :: (String,CMI_SFZ) -> String
-cmi_sfz_pp (nm,(l1,l2,l3,e1,e2)) = printf "%-24s %c %5d %5d %3.1f %3.1f" nm l1 l2 l3 e1 e2
+cmi_sfz_pp (nm,(_,_,l1,l2,l3,e1,e2)) = printf "%-24s %c %5d %5d %3.1f %3.1f" nm l1 l2 l3 e1 e2
 
 -- | Load all .sfz files below directory.
 --   Names are of the form DISK/VOICE.
