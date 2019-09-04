c0a61a9c3a9f62ca319130b03f2645b6201078c9
rd <rd@slavepianos.org>
2019-09-04 11:34:45 +0000

    ~cmi,sf

--- Sound/SC3/Data/Fairlight/CMI.hs
+++ Sound/SC3/Data/Fairlight/CMI.hs
@@ -20,50 +20,49 @@ import Sound.File.HSndFile {- hsc3-sf-sndfile -}
 
 import Sound.SC3.Data.SFZ {- hsc3-data -}
 
--- | (FILENAME,VOLUME,KEY-CENTER,LOOP-MODE-SYM,LOOP-START,LOOP-END,EG-ATTACK,EG-RELEASE)
+-- | (DIR,FILENAME,VOLUME,KEY-CENTER,LOOP-MODE-SYM,LOOP-START,LOOP-END,EG-ATTACK,EG-RELEASE)
 --
 -- For CMI in all cases VOLUME=-3 ; KEYCENTER=A3=57
-type CMI_SFZ = (FilePath, Double, Word8, Char, Int, Int, Double, Double)
+type CMI_SFZ = (FilePath,FilePath,Double, Word8, Char, Int, Int, Double, Double)
 
 -- | Parse SFZ <region>.
-cmi_sfz_rgn_parse :: SFZ_Region -> CMI_SFZ
-cmi_sfz_rgn_parse r =
+cmi_sfz_rgn_parse :: FilePath -> SFZ_Region -> CMI_SFZ
+cmi_sfz_rgn_parse dir r =
   let vol = sfz_region_volume r
       mnn = sfz_region_pitch_keycenter r
       lp_mode = fromMaybe (error "cmi_sfz_rgn_parse?") (sfz_region_loop_mode_sym r)
-  in (sfz_region_sample r,vol,mnn,lp_mode,sfz_region_loop_start r,sfz_region_loop_end r
+  in (dir,sfz_region_sample r,vol,mnn,lp_mode,sfz_region_loop_start r,sfz_region_loop_end r
      ,sfz_region_ampeg_attack r,sfz_region_ampeg_release r)
 
--- | SFZ and SND data.
-type CMI_DAT = ([SFZ_Region],SF_Header)
+-- | SFZ region, directory of SFZ file, SND header.
+type CMI_DAT = (SFZ_Region,FilePath,SF_Header)
 
 -- | Parse SFZ <region>.
 cmi_sfz_parse :: CMI_DAT -> CMI_SFZ
-cmi_sfz_parse l =
-  case l of
-    ([r],_) -> cmi_sfz_rgn_parse r
-    _ -> error "cmi_sfz_parse?"
+cmi_sfz_parse (r,dir,_) = cmi_sfz_rgn_parse dir r
 
 -- | Load SFZ and SND-HEADER data.
-cmi_load_dat :: String -> FilePath -> String -> IO CMI_DAT
-cmi_load_dat ext dir nm = do
-  (_,_,sfz_rgn) <- sfz_load (dir </> nm <.> "sfz")
-  snd_hdr <- sf_header (dir </> nm <.> ext)
-  return (sfz_rgn,snd_hdr)
+cmi_load_dat :: FilePath -> String -> IO CMI_DAT
+cmi_load_dat cmi_dir fn = do
+  (_,_,[rgn]) <- sfz_load (cmi_dir </> fn)
+  let dir = takeDirectory (cmi_dir </> fn)
+      sf_fn = dir </> sfz_region_sample rgn
+  hdr <- sf_header sf_fn
+  return (rgn,dir,hdr)
 
 -- | Load and parse SFZ.
-cmi_load_sfz :: String -> FilePath -> String -> IO CMI_SFZ
-cmi_load_sfz ext dir = fmap cmi_sfz_parse . cmi_load_dat ext dir
+cmi_load_sfz :: FilePath -> String -> IO CMI_SFZ
+cmi_load_sfz dir = fmap cmi_sfz_parse . cmi_load_dat dir
 
 -- | Pretty-printer.
 cmi_sfz_pp :: (String,CMI_SFZ) -> String
-cmi_sfz_pp (nm,(_,_,_,l1,l2,l3,e1,e2)) = printf "%-24s %c %5d %5d %3.1f %3.1f" nm l1 l2 l3 e1 e2
+cmi_sfz_pp (nm,(_,_,_,_,l1,l2,l3,e1,e2)) = printf "%-24s %c %5d %5d %3.1f %3.1f" nm l1 l2 l3 e1 e2
 
 -- | Load all .sfz files below directory.
 --   Names are of the form DISK/VOICE.
-cmi_load_dir :: String -> FilePath -> IO [(FilePath, CMI_SFZ)]
-cmi_load_dir ext cmi_dir = do
+cmi_load_dir :: FilePath -> IO [(FilePath, CMI_SFZ)]
+cmi_load_dir cmi_dir = do
   fn <- dir_find_ext_rel ".sfz" cmi_dir
-  let nm_seq = sort (map dropExtension fn)
-  cmi_seq <- mapM (cmi_load_sfz ext cmi_dir) nm_seq
+  let nm_seq = map dropExtension (sort fn)
+  cmi_seq <- mapM (cmi_load_sfz cmi_dir) (sort fn)
   return (zip nm_seq cmi_seq)
