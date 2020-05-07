b9c34b689f389747a3fbc61aed6171c6b4868d0a
rd <rd@slavepianos.org>
2020-05-07 03:12:12 +0000

    ~pb/interp

--- Sound/SC3/Data/Math/Bourke.hs
+++ Sound/SC3/Data/Math/Bourke.hs
@@ -1,20 +1,24 @@
 -- | <http://paulbourke.net/>
 module Sound.SC3.Data.Math.Bourke where
 
+import Data.CG.Minus.Plain {- hcg-minus -}
+
+-- * FRACTALS <http://paulbourke.net/fractals>
+
 -- | 2-element /h/ transform.
-h_transform_2 :: Num t => t -> ((t,t) -> (t,t)) -> (t,t) -> (t,t)
+h_transform_2 :: Num t => t -> (V2 t -> V2 t) -> V2 t -> V2 t
 h_transform_2 h f (x,y) =
     let (x',y') = f (x,y)
     in (x + h * x',y + h * y')
 
 -- | 3-element /h/ transform.
-h_transform_3 :: Num t => t -> ((t,t,t) -> (t,t,t)) -> (t,t,t) -> (t,t,t)
+h_transform_3 :: Num t => t -> (V3 t -> V3 t) -> V3 t -> V3 t
 h_transform_3 h f (x,y,z) =
     let (x',y',z') = f (x,y,z)
     in (x + h * x',y + h * y',z + h * z')
 
 -- | <http://paulbourke.net/fractals/lorenz/>
-lorenz :: Num t => t -> t -> t -> (t,t,t) -> (t,t,t)
+lorenz :: Num t => t -> t -> t -> V3 t -> V3 t
 lorenz a b c (x,y,z) =
     (a * (y - x)
     ,x * (b - z) - y
@@ -24,19 +28,19 @@ lorenz a b c (x,y,z) =
 
 > import Sound.SC3.Plot {- hsc3-plot -}
 
-> let l = iterate (lorenz_h 0.01 10 28 (8/3)) (0.1,0.0,0.0)
-> in plot_p3_ln [take 5000 l]
+> l = iterate (lorenz_h 0.01 10 28 (8/3)) (0.1,0.0,0.0)
+> plot_p3_ln [take 5000 l]
 
-> let {l = iterate (lorenz_h 0.01 10 28 (8/3)) (0.1,0.0,0.0)
->     ;f (x,_,z) = (x,z)}
-> in plot_p2_ln [take 15000 (map f l)]
+> l = iterate (lorenz_h 0.01 10 28 (8/3)) (0.1,0.0,0.0)
+> f (x,_,z) = (x,z)
+> plot_p2_ln [take 15000 (map f l)]
 
 -}
-lorenz_h :: Num t => t -> t -> t -> t -> (t,t,t) -> (t,t,t)
+lorenz_h :: Num t => t -> t -> t -> t -> V3 t -> V3 t
 lorenz_h h a b c = h_transform_3 h (lorenz a b c)
 
 -- | <http://paulbourke.net/fractals/rossler/>
-rossler :: Num t => t -> t -> t -> (t, t, t) -> (t, t, t)
+rossler :: Num t => t -> t -> t -> V3 t -> V3 t
 rossler a b c (x,y,z) =
     (negate y - z
     ,x + a * y
@@ -47,14 +51,13 @@ rossler a b c (x,y,z) =
 > plot_p3_ln [take 5000 (iterate (rossler_h 0.02 0.2 0.2 5.7) (0.1,0,0))]
 
 -}
-rossler_h :: Num t => t -> t -> t -> t -> (t,t,t) -> (t,t,t)
+rossler_h :: Num t => t -> t -> t -> t -> V3 t -> V3 t
 rossler_h h a b c = h_transform_3 h (rossler a b c)
 
 {- | <http://paulbourke.net/fractals/peterdejong/>
 
-> let pdj a b c d =
->   let vw x = plot_p2_pt [take 15000 x]
->   in vw (iterate (peter_de_jong a b c d) (-0.72,-0.64))
+> vw x = plot_p2_pt [take 15000 x]
+> pdj a b c d = vw (iterate (peter_de_jong a b c d) (-0.72,-0.64))
 
 > pdj 1.4 (-2.3) 2.4 (-2.1)
 > pdj 2.01 (-2.53) 1.61 (-0.33)
@@ -63,16 +66,15 @@ rossler_h h a b c = h_transform_3 h (rossler a b c)
 > pdj (-2.0) (-2.0) (-1.2) 2.0
 
 -}
-peter_de_jong :: Floating t => t -> t -> t -> t -> (t, t) -> (t, t)
+peter_de_jong :: Floating t => t -> t -> t -> t -> V2 t -> V2 t
 peter_de_jong a b c d (x,y) =
     (sin (a * y) - cos (b * x)
     ,sin (c * x) - cos (d * y))
 
 {- | <http://paulbourke.net/fractals/clifford/>
 
-> let clf a b c d =
->   let vw x = plot_p2_pt [take 12500 x]
->   in vw (iterate (clifford a b c d) (-0.72,-0.64))
+> vw x = plot_p2_pt [take 12500 x]
+> clf a b c d = vw (iterate (clifford a b c d) (-0.72,-0.64))
 
 > clf (-1.4) (1.6) (1.0) (0.7)
 > clf (1.1) (-1.0) (1.0) (1.5) {- not as pb indicates -}
@@ -80,7 +82,76 @@ peter_de_jong a b c d (x,y) =
 > clf (1.7) (1.7) (0.6) (1.2)
 
 -}
-clifford :: Floating t => t -> t -> t -> t -> (t, t) -> (t, t)
+clifford :: Floating t => t -> t -> t -> t -> V2 t -> V2 t
 clifford a b c d (x,y) =
     (sin (a * y) + c * cos (a * x)
     ,sin (b * x) + d * cos (b * y))
+
+-- * INTERPOLATION
+
+-- | <http://paulbourke.net/miscellaneous/interpolation/>
+interpolate_linear :: Num a => V2 a -> a -> a
+interpolate_linear (y1,y2) mu = (y1 * (1 - mu)) + (y2 * mu)
+
+interpolate_cosine :: Floating a => V2 a -> a -> a
+interpolate_cosine (y1,y2) mu =
+  let mu2 = (1 - cos (mu * pi)) / 2
+  in (y1 * (1 - mu2) + y2 * mu2)
+
+interpolate_cubic :: Num a => V4 a -> a -> a
+interpolate_cubic (y0,y1,y2,y3) mu =
+  let mu2 = mu * mu
+      a0 = y3 - y2 - y0 + y1
+      a1 = y0 - y1 - a0
+      a2 = y2 - y0
+      a3 = y1
+  in a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3
+
+interpolate_catmull_rom :: Fractional a => V4 a -> a -> a
+interpolate_catmull_rom (y0,y1,y2,y3) mu =
+  let mu2 = mu * mu
+      a0 = (-0.5) * y0 + 1.5 * y1 - 1.5 * y2 + 0.5 * y3
+      a1 = y0 - 2.5 * y1 + 2 * y2 - 0.5 * y3
+      a2 = (-0.5) * y0 + 0.5 * y2
+      a3 = y1
+  in a0 * mu * mu2 + a1 * mu2 + a2 * mu + a3
+
+{- | Hermite interpolation
+
+   Tension: 1 is high, 0 normal, -1 is low
+   Bias: 0 is even,
+         positive is towards first segment,
+         negative towards the other
+-}
+interpolate_hermite :: Fractional a => (a,a) -> V4 a -> a -> a
+interpolate_hermite (tension,bias) (y0,y1,y2,y3) mu =
+ let mu2 = mu * mu
+     mu3 = mu2 * mu
+     m0 = ((y1 - y0) * (1 + bias) * (1 - tension)/2) + ((y2 - y1) * (1 - bias) * (1 - tension)/2)
+     m1 = ((y2 - y1) * (1 + bias) * (1 - tension)/2) + ((y3 - y2) * (1 - bias) * (1 - tension)/2)
+     a0 = 2 * mu3 - 3 * mu2 + 1
+     a1 = mu3 - 2 * mu2 + mu
+     a2 = mu3 - mu2
+     a3 = (-2) * mu3 + 3 * mu2
+ in a0 * y1 + a1 * m0 + a2 * m1 + a3 * y2
+
+{-
+
+import qualified Music.Theory.List as T {- hmt -}
+import Sound.SC3.Plot {- hsc3-plot -}
+
+y = [145,60,75,55,155,0,100,25]
+m k = take k [0,1 / fromIntegral k ..]
+z2 k f = concatMap (\z -> map (f z) (m k)) (T.adj2 1 y)
+plot_p1_ln [z2 100 interpolate_linear,z2 100 interpolate_cosine]
+
+y' = [145,145,60,75,55,155,0,100,25,25]
+z4 k f = concatMap (\[y1,y2,y3,y4] -> map (f (y1,y2,y3,y4)) (m k)) (T.adj_trunc 4 1 y')
+plot_p1_ln [z4 100 interpolate_cubic,z4 100 interpolate_catmull_rom,z4 100 (interpolate_hermite (0,0))]
+plot_p1_ln [z4 100 (interpolate_hermite (t,b)) | t <- [0,1], b <- [0,1]]
+plot_p1_ln [z4 100 (interpolate_hermite (t,b)) | t <- [-1,0], b <- [-1,0]]
+plot_p1_ln [z4 100 (interpolate_hermite (t,b)) | t <- [-1,0,1], b <- [-1,0,1]]
+plot_p1_ln [z4 100 (interpolate_hermite (t,b)) | t <- [-0.5,0,0.5], b <- [-0.5,0,0.5]]
+plot_p1_ln [z4 100 (interpolate_hermite (t,b)) | t <- [-2,2], b <- [0,0.15]]
+
+-}
--- Sound/SC3/Data/Math/Hermite.hs
+++ Sound/SC3/Data/Math/Hermite.hs
@@ -1,18 +1,25 @@
+-- | Hermite bases and functions.
 module Sound.SC3.Data.Math.Hermite where
 
--- | 4-point, 3rd-order Hermite (x-form)
-hermite_p4_o3_xf :: Fractional t => t -> (t,t,t,t) -> t
-hermite_p4_o3_xf x (y0,y1,y2,y3) =
-    let c0 = y1;
-        c1 = 0.5 * (y2 - y0)
-        c2 = y0 - 2.5 * y1 + 2.0 * y2 - 0.5 * y3
-        c3 = 1.5 * (y1 - y2) + 0.5 * (y3 - y0)
-    in ((c3 * x + c2) * x + c1) * x + c0
+import Data.CG.Minus.Plain {- hcg-minus -}
 
--- | Hermite bases, expanded form
---
--- > map hermite_bases_exp [0,1/4,1/2,3/4,1]
-hermite_bases_exp :: Num d => d -> (d,d,d,d)
+-- | Hermite data set in form ((p0,p1),(m0,m1)).
+type HERMITE n = V2 (V2 n)
+
+-- | Bezier data set in form (p0,p1,p2,p3)
+type BEZIER n = V4 n
+
+-- | Convert HERMITE to bezier data of form (p0,p1,p2,p3).
+hermite_to_bezier :: Fractional n => HERMITE n -> BEZIER n
+hermite_to_bezier ((p0,p1),(m0,m1)) = (p0,p0 + (m0 / 3),p1 - (m1 / 3),p1)
+
+{- | Hermite bases, expanded form.
+
+> f = v4_map (round . (*) 100)
+> r = [(100,0,0,0),(84,14,16,-5),(50,12,50,-12),(16,5,84,-14),(0,0,100,0)]
+> map (f . hermite_bases_exp) [0,1/4,1/2,3/4,1] == r
+-}
+hermite_bases_exp :: Num n => n -> V4 n
 hermite_bases_exp t =
   let cub n = n * n * n
       sqr n = n * n
@@ -22,10 +29,11 @@ hermite_bases_exp t =
       h11 = cub t - sqr t -- sq t * (t - 1)
   in (h00,h10,h01,h11)
 
--- | Hermite bases, factored form.
---
--- > map hermite_bases_fac [0,1/4,1/2,3/4,1]
-hermite_bases_fac :: Num d => d -> (d,d,d,d)
+{- | Hermite bases, factored form.
+
+> map (f . hermite_bases_fac) [0,1/4,1/2,3/4,1] == r
+-}
+hermite_bases_fac :: Num n => n -> V4 n
 hermite_bases_fac t =
   let sqr n = n * n
       h00 = (1 + 2 * t) * sqr (1 - t)
@@ -34,10 +42,24 @@ hermite_bases_fac t =
       h11 = sqr t * (t - 1)
   in (h00,h10,h01,h11)
 
-hermite :: Num a => (a, a) -> (a, a) -> a -> a
-hermite (p0,p1) (m0,m1) t =
+-- | Evaluate HERMITE at /t/.
+hermite :: Num n => HERMITE n -> n -> n
+hermite ((p0,p1),(m0,m1)) t =
   let (h00,h10,h01,h11) = hermite_bases_exp t
   in h00 * p0 + h10 * m0 + h01 * p1 + h11 * m1
+
+{-
+-- | 4-point, 3rd-order Hermite (x-form)
+hermite_p4_o3_xf :: Fractional t => t -> (t,t,t,t) -> t
+hermite_p4_o3_xf x (y0,y1,y2,y3) =
+    let c0 = y1;
+        c1 = 0.5 * (y2 - y0)
+        c2 = y0 - 2.5 * y1 + 2.0 * y2 - 0.5 * y3
+        c3 = 1.5 * (y1 - y2) + 0.5 * (y3 - y0)
+    in ((c3 * x + c2) * x + c1) * x + c0
+-}
+
+-- * KOCHANEK-BARTELS
 
 {- | <https://en.wikipedia.org/wiki/Kochanek-Bartels_spline>
 
@@ -45,9 +67,9 @@ t = tension = changes the length of the tangent vector
 b = bias = primarily changes the direction of the tangent vector
 c = continuity = changes the sharpness in change between tangents
 
-Setting each parameter to zero would give a Catmull–Rom spline.
+Setting each parameter to zero would give a Catmull-Rom spline.
 -}
-kochanek_bartels_spline_m :: Fractional t => (t,t,t) -> (t,t,t,t) -> (t,t)
+kochanek_bartels_spline_m :: Fractional t => V3 t -> V4 t -> V2 t
 kochanek_bartels_spline_m (t,b,c) (p0,p1,p2,p3) =
   let m1 = ((((1 - t) * (1 + b) * (1 + c)) / 2) * (p1 - p0)) +
            ((((1 - t) * (1 - b) * (1 - c)) / 2) * (p2 - p1))
@@ -56,7 +78,7 @@ kochanek_bartels_spline_m (t,b,c) (p0,p1,p2,p3) =
            ((((1 - t) * (1 - b) * (1 + c)) / 2) * (p3 - p2))
   in (m1,m2)
 
-kochanek_bartels_spline_h :: Fractional t => (t,t,t) -> [t] -> [((t,t),(t,t))]
+kochanek_bartels_spline_h :: Fractional t => V3 t -> [t] -> [HERMITE t]
 kochanek_bartels_spline_h param x =
   let rpt_bnd l = head l : l ++ [last l]
       adj4 l = case l of {p:q:r:s:_ -> (p,q,r,s) : adj4 (tail l) ; _ -> []}
@@ -65,11 +87,20 @@ kochanek_bartels_spline_h param x =
       m = map (kochanek_bartels_spline_m param) p4
   in zip p2 m
 
-hermite_to_bezier :: Fractional n => ((n,n),(n,n)) -> (n,n,n,n)
-hermite_to_bezier ((p0,p1),(m0,m1)) = (p0,p0 + (m0 / 3),p1 - (m1 / 3),p1)
-
-kochanek_bartels_spline_b :: Fractional t => (t,t,t) -> [t] -> [(t,t,t,t)]
+kochanek_bartels_spline_b :: Fractional t => V3 t -> [t] -> [BEZIER t]
 kochanek_bartels_spline_b param = map hermite_to_bezier . kochanek_bartels_spline_h param
+
+kochanek_bartels_spline_v2_b :: Fractional n => V3 n -> [V2 n] -> [BEZIER (V2 n)]
+kochanek_bartels_spline_v2_b param pt =
+  let (x,y) = unzip pt
+      f = kochanek_bartels_spline_b param
+  in zipWith (v4_zip (,)) (f x) (f y)
+
+kochanek_bartels_spline_v3_b :: Fractional n => V3 n -> [V3 n] -> [BEZIER (V3 n)]
+kochanek_bartels_spline_v3_b param pt =
+  let (x,y,z) = unzip3 pt
+      f = kochanek_bartels_spline_b param
+  in zipWith3 (v4_zip3 (,,)) (f x) (f y) (f z)
 
 {-
 
@@ -78,7 +109,7 @@ import Sound.SC3.Plot {- hsc3-plot -}
 y = [145,60,75,55,155,0,100,25]
 x k = take k [0,1 / fromIntegral k ..]
 h o = kochanek_bartels_spline_h o y
-f k (p,m) = map (hermite p m) (x k)
+f k d = map (hermite d) (x k)
 r o = concatMap (f 50) (h o)
 plot_p1_ln [r (0,0,0),r (1,0,0),r (0,1,0),r (0,0,1),r (0,0,0),r (-1,0,0),r (0,-1,0),r (0,0,-1)]
 
