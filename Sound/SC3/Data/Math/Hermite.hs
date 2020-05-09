93d91e025e818a5c8990a367686b607a7709ec03
rd <rd@slavepianos.org>
2020-05-09 08:56:17 +0000

    +cardinal_spline

--- Sound/SC3/Data/Math/Hermite.hs
+++ Sound/SC3/Data/Math/Hermite.hs
@@ -18,6 +18,11 @@ hermite_to_bezier ((p0,p1),(m0,m1)) = (p0,p0 + (m0 / 3),p1 - (m1 / 3),p1)
 > f = v4_map (round . (*) 100)
 > r = [(100,0,0,0),(84,14,16,-5),(50,12,50,-12),(16,5,84,-14),(0,0,100,0)]
 > map (f . hermite_bases_exp) [0,1/4,1/2,3/4,1] == r
+
+> import Sound.SC3.Plot {- hsc3-plot -}
+> (p,q,r,s) = Data.List.unzip4 (map hermite_bases_exp [0,0.01 .. 1])
+> plot_p1_ln [p,q,r,s]
+
 -}
 hermite_bases_exp :: Num n => n -> V4 n
 hermite_bases_exp t =
@@ -69,26 +74,23 @@ c = continuity = changes the sharpness in change between tangents
 
 Setting each parameter to zero would give a Catmull-Rom spline.
 -}
-kochanek_bartels_spline_m :: Fractional t => V3 t -> V4 t -> V2 t
-kochanek_bartels_spline_m (t,b,c) (p0,p1,p2,p3) =
+kochanek_bartels_spline_h :: Fractional t => V3 t -> V4 t -> HERMITE t
+kochanek_bartels_spline_h (t,b,c) (p0,p1,p2,p3) =
   let m1 = ((((1 - t) * (1 + b) * (1 + c)) / 2) * (p1 - p0)) +
            ((((1 - t) * (1 - b) * (1 - c)) / 2) * (p2 - p1))
 
       m2 = ((((1 - t) * (1 + b) * (1 - c)) / 2) * (p2 - p1)) +
            ((((1 - t) * (1 - b) * (1 + c)) / 2) * (p3 - p2))
-  in (m1,m2)
+  in ((p1,p2),(m1,m2))
 
-kochanek_bartels_spline_h :: Fractional t => V3 t -> [t] -> [HERMITE t]
-kochanek_bartels_spline_h param x =
+kochanek_bartels_spline :: Fractional t => V3 t -> [t] -> [HERMITE t]
+kochanek_bartels_spline o =
   let rpt_bnd l = head l : l ++ [last l]
       adj4 l = case l of {p:q:r:s:_ -> (p,q,r,s) : adj4 (tail l) ; _ -> []}
-      p4 = adj4 (rpt_bnd x)
-      p2 = map (\(_,i,j,_) -> (i,j)) p4
-      m = map (kochanek_bartels_spline_m param) p4
-  in zip p2 m
+  in map (kochanek_bartels_spline_h o) . adj4 . rpt_bnd
 
 kochanek_bartels_spline_b :: Fractional t => V3 t -> [t] -> [BEZIER t]
-kochanek_bartels_spline_b param = map hermite_to_bezier . kochanek_bartels_spline_h param
+kochanek_bartels_spline_b param = map hermite_to_bezier . kochanek_bartels_spline param
 
 kochanek_bartels_spline_v2_b :: Fractional n => V3 n -> [V2 n] -> [BEZIER (V2 n)]
 kochanek_bartels_spline_v2_b param pt =
@@ -102,15 +104,41 @@ kochanek_bartels_spline_v3_b param pt =
       f = kochanek_bartels_spline_b param
   in zipWith3 (v4_zip3 (,,)) (f x) (f y) (f z)
 
+-- * CARDINAL
+
+cardinal_spline_h :: Num n => n -> V4 n -> HERMITE n
+cardinal_spline_h c (p0,p1,p2,p3) =
+  let m (i,j) = (1 - c) * (j - i)
+  in ((p1,p2),(m (p0,p2),m (p1,p3)))
+
+cardinal_spline :: Num n => n -> [n] -> [HERMITE n]
+cardinal_spline c =
+  let rpt_bnd l = head l : l ++ [last l]
+      adj4 l = case l of {p:q:r:s:_ -> (p,q,r,s) : adj4 (tail l) ; _ -> []}
+  in map (cardinal_spline_h c) . adj4 . rpt_bnd
+
+
 {-
 
 import Sound.SC3.Plot {- hsc3-plot -}
 
 y = [145,60,75,55,155,0,100,25]
 x k = take k [0,1 / fromIntegral k ..]
-h o = kochanek_bartels_spline_h o y
 f k d = map (hermite d) (x k)
-r o = concatMap (f 50) (h o)
+
+h o = kochanek_bartels_spline o y
+r = concatMap (f 50) . h
 plot_p1_ln [r (0,0,0),r (1,0,0),r (0,1,0),r (0,0,1),r (0,0,0),r (-1,0,0),r (0,-1,0),r (0,0,-1)]
+
+h c = cardinal_spline c y
+r = concatMap (f 50) . h
+plot_p1_ln (map r [0,0.125,0.25,0.5,1])
+plot_p1_ln (map r [0,0.1 .. 1])
+
+y = [10,0,10,100,90,0,0,10,0,0]
+y = [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
+h c = cardinal_spline c y
+r = concatMap (f 50) . h
+plot_p1_ln (map r [0,0.125,0.25,0.5,1])
 
 -}
