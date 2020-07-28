95a5fd2e2972503d193fec3e10e9719ae940af37
rd <rd@slavepianos.org>
2020-07-28 00:06:58 +0000

    ~tutte_gen

--- Sound/SC3/Data/Geometry/Tutte.hs
+++ Sound/SC3/Data/Geometry/Tutte.hs
@@ -45,15 +45,21 @@ tutte_step adj fc v =
   let f (i,j) = if i `elem` fc then (i,j) else (i,v_loc_centre v (T.adj_mtx_con (0,1) adj i))
   in map f v
 
--- | Generate sequence of Tuttes of /o/
-tutte_gen :: OFF.OFF3 R -> Int -> ([V_LOC],[V2 Int])
-tutte_gen o i =
-  let ((k,_),(_,fc)) = o
-      gr = T.lbl_to_g (OFF.off_graph o)
-      adj = T.edg_to_adj_mtx_undir (0,1) (T.g_to_edg gr)
-      (_,fc_i) = fc !! i
+-- | Generate sequence of Tuttes given graph, face list and outer face index.
+tutte_gen :: T.G -> [[Int]] -> Int -> ([V_LOC],[V2 Int])
+tutte_gen (v,e) fc i =
+  let k = length v
+      adj = T.edg_to_adj_mtx_undir (0,1) (T.g_to_edg (v,e))
+      fc_i = fc !! i
       v0 = v_init_loc k fc_i
-  in (iterate (tutte_step adj fc_i) v0,snd gr)
+  in (iterate (tutte_step adj fc_i) v0,e)
+
+-- | 'tutte_gen' of OFF3
+tutte_gen_off3 :: OFF.OFF3 R -> Int -> ([V_LOC],[V2 Int])
+tutte_gen_off3 o i =
+  let gr = T.lbl_to_g (OFF.off_graph o)
+      ((_,_),(_,fc)) = o
+  in tutte_gen gr (map snd fc) i
 
 -- | Store tutte to OBJ file.
 tutte_obj :: FilePath -> V_LOC -> [V2 Int] -> IO ()
