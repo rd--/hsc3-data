89698245f456cb992c82747e5a4022e6abae3dd0
rohan <rd@slavepianos.org>
2022-08-16 16:10:27 +1000

    ~Markdown,+commetns

--- Sound/Sc3/Data/Text/Markdown.hs
+++ Sound/Sc3/Data/Text/Markdown.hs
@@ -1,15 +1,19 @@
+-- | Simple markdown utilities
 module Sound.Sc3.Data.Text.Markdown where
 
 import Safe {- safe -}
 
 import Music.Theory.List {- hmt-base -}
 
+-- | Is a line a top-level heading.
 md_is_h1 :: String -> Bool
 md_is_h1 = (== "#") . headDef "" . words
 
+-- | Split a markdown text (given as a sequence of lines) into top-level sections.
 md_split_at_h1 :: [String] -> [[String]]
 md_split_at_h1 = filter (not . null) . split_when_keeping_left md_is_h1
 
+-- | Read a markdown file and write each top-level section separately.
 md_extract_h1 :: FilePath -> ([String] -> FilePath) -> IO ()
 md_extract_h1 in_file gen_fn = do
   in_ln <- fmap lines (readFile in_file)
